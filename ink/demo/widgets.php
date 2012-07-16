1a74133223f9e212edaf85e169bacff893df2737
Pedro Couto e Santos <psantos@co.sapo.pt>
2012-07-16 17:49:58 +0100

    Added basic widgets page

---
+++ ink/demo/widgets.php
@@ -0,0 +1,53 @@
+<?php include 'shared/header.php'; ?>
+<!-- |||||||||||||||||||||||||||||||||  Navitation  |||||||||||||||||||||||||||||||||  -->
+<div class="blackMenu" id="topMenu">
+	 <h1><a href="intro.html" title="Site Title">InK <small>Interface kit</small></a></h1>
+	<nav>
+	<ul class="h_navigation">
+		<li><a href="intro.html">Intro</a></li>
+		<li><a href="grid.html">Grelha</a></li>
+		<li><a href="typo.html">Tipografia</a></li>
+		<li><a href="forms.html">Formulários</a></li>
+		<li><a href="tables.html">Tabelas</a></li>
+		<li><a href="alerts.html">Alerts</a></li>
+		<li><a href="navigation.html">Navegação</a></li>
+		<li><a href="widgets.html" class="active">Widgets</a></li>
+	</ul>
+	</nav>
+	<a href="#" onclick="toogleNav()" id="toggleNavigation">Menu</a>
+</div>
+<!-- |||||||||||||||||||||||||||||||||  Content  |||||||||||||||||||||||||||||||||  -->		
+
+<div class="container_width">
+	<h2><span>Widgets</span></h2>
+	<div class="space">
+		O Ink fornece um conjunto de 16 widgets em Javascript especialmente preparados para serem de fácil utilização para pessoas que possam ter pouca experiência em JS.
+	</div>
+	
+	<div class="section">
+		<div class="space">
+		<h3>Widgets disponíveis</h3>
+		<p>Clique para ver exemplos e instruções de utilização.</p>
+			<div class="g25"><h4><a href="">Menu Contextual</a></h4>
+				<a href=""><img src="styles/imgs/wdgt_context_menu.jpg" /></a>
+				<p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">Selector de data</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">Diálogo</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">Galeria</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">'Lightbox'</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">Medidor de Senha</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">ProxyGallery</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">Slidebox</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">SortableList</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">Table</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">Tabs</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">Tags</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">Tooltip</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">MaskedInput</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">Resizable</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+			<div class="g25"><h4><a href="">TimePicker</a></h4><a href=""><img src="styles/imgs/screenshot.jpg" /></a><p><a href="">Este widget permite fazer cenas porreiras com JavaScript nas páginas, como coiso e tal e não sei o quê.</a></p></div>
+		</div>
+	</div>
+	
+</div>
+<?php include 'shared/footer.php'; ?>
