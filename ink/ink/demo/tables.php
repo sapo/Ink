8a437fa3fe2606eb4574c6fe1b8ab651aa931536
Pedro Correia <pedro.correia@gmail.com>
2012-09-04 18:48:25 +0100

    Adds table styles: default, zebra, hover rows, all borders

--- ink/ink/demo/tables.php
+++ ink/ink/demo/tables.php
@@ -2,19 +2,36 @@
 <!-- |||||||||||||||||||||||||||||||||  Navitation  |||||||||||||||||||||||||||||||||  -->
 <nav id="topbar">
 	<div class="ink-container">
-		<h1><a href="intro.php" title="Site Title">InK<small>Interface kit</small></a></h1>
+		<h1>
+			<a href="intro.php" title="Site Title">
+				InK
+				<small>Interface kit</small>
+			</a>
+		</h1>
 		<ul>
-			<li><a href="grid.php">Layout</a></li>
-			<li><a href="navigation.php">Navigation</a></li>
-			<li><a href="typo.php">Typography & Icons</a></li>
-			<li><a href="forms.php">Forms & Alerts</a></li>
-			<li class="active"><a href="tables.php">Tables</a></li>
-			<li><a href="widgets.php">InkJS</a></li>
+			<li>
+				<a href="grid.php">Layout</a>
+			</li>
+			<li>
+				<a href="navigation.php">Navigation</a>
+			</li>
+			<li>
+				<a href="typo.php">Typography & Icons</a>
+			</li>
+			<li>
+				<a href="forms.php">Forms & Alerts</a>
+			</li>
+			<li class="active">
+				<a href="tables.php">Tables</a>
+			</li>
+			<li>
+				<a href="widgets.php">InkJS</a>
+			</li>
 		</ul>
 	</div>
-</nav>  
+</nav>
 
-<!-- |||||||||||||||||||||||||||||||||  Content  |||||||||||||||||||||||||||||||||  -->		
+<!-- |||||||||||||||||||||||||||||||||  Content  |||||||||||||||||||||||||||||||||  -->
 <div class="ink-container whatIs">
 	<div class="ink-space">
 		<h2>Navigation</h2>
@@ -25,56 +42,168 @@
 <nav class="menu">
 	<div class="ink-container">
 		<ul>
-			<li class="active"><a href="#">Simple</a></li>
-			<li><a href="#">Borderless</a></li>
-			<li><a href="#">Sortable</a></li>
+			<li class="active">
+				<a href="#">Simple</a>
+			</li>
+			<li>
+				<a href="#">Borderless</a>
+			</li>
+			<li>
+				<a href="#">Sortable</a>
+			</li>
 		</ul>
 	</div>
-</nav>  
+</nav>
 
 <div class="ink-container">
 	<div class="ink-g25">
 		<div class="ink-space">
 			<h3>Tabelas</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		</div>					
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</div>
 	</div>
 	<div class="ink-g75">
 		<div class="ink-space">
-			<table>
-				<tr>
-					<th>Table Header</th>
-					<th>Table Header</th>
-					<th>Table Header</th>
-				</tr>
-				<tr>
-					<td>Table Content</td>
-					<td>Table Content</td>
-					<td>Table Content</td>
-				</tr>
-				<tr>
-					<td>Table Content</td>
-					<td>Table Content</td>
-					<td>Table Content</td>
-				</tr>
-				<tr>
-					<td>Table Content</td>
-					<td>Table Content</td>
-					<td>Table Content</td>
-				</tr>
-				<tr>
-					<td>Table Content</td>
-					<td>Table Content</td>
-					<td>Table Content</td>
-				</tr>
-				<tr>
-					<td>Table Content</td>
-					<td>Table Content</td>
-					<td>Table Content</td>
-				</tr>
-			</table>
+			<div class="ink-g100 ink-vspace">
+				<h4>Default table style</h4>
+				<table class="ink-table">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Product name</th>
+							<th>Product price</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>24</td>
+							<td>Twin color basketball shoe</td>
+							<td>&euro;26,00</td>
+						</tr>
+						<tr>
+							<td>13</td>
+							<td>Cast iron waffle maker</td>
+							<td>&euro;158,00</td>
+						</tr>
+						<tr>
+							<td>55</td>
+							<td>Sports duffle bag</td>
+							<td>&euro;15,00</td>
+						</tr>
+						<tr>
+							<td>23</td>
+							<td>Some stuff </td>
+							<td>&euro;489,00</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="ink-g100 ink-vspace">
+				<h4>Zebra table</h4>
+				<table class="ink-table ink-zebra">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Product name</th>
+							<th>Product price</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>24</td>
+							<td>Twin color basketball shoe</td>
+							<td>&euro;26,00</td>
+						</tr>
+						<tr>
+							<td>13</td>
+							<td>Cast iron waffle maker</td>
+							<td>&euro;158,00</td>
+						</tr>
+						<tr>
+							<td>55</td>
+							<td>Sports duffle bag</td>
+							<td>&euro;15,00</td>
+						</tr>
+						<tr>
+							<td>23</td>
+							<td>Some stuff </td>
+							<td>&euro;489,00</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="ink-g100 ink-vspace">
+				<h4>Hover rows table</h4>
+				<table class="ink-table ink-hover">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Product name</th>
+							<th>Product price</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>24</td>
+							<td>Twin color basketball shoe</td>
+							<td>&euro;26,00</td>
+						</tr>
+						<tr>
+							<td>13</td>
+							<td>Cast iron waffle maker</td>
+							<td>&euro;158,00</td>
+						</tr>
+						<tr>
+							<td>55</td>
+							<td>Sports duffle bag</td>
+							<td>&euro;15,00</td>
+						</tr>
+						<tr>
+							<td>23</td>
+							<td>Some stuff </td>
+							<td>&euro;489,00</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="ink-g100 ink-vspace">
+				<h4>Hover rows table</h4>
+				<table class="ink-table ink-columns">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Product name</th>
+							<th>Product price</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>24</td>
+							<td>Twin color basketball shoe</td>
+							<td>&euro;26,00</td>
+						</tr>
+						<tr>
+							<td>13</td>
+							<td>Cast iron waffle maker</td>
+							<td>&euro;158,00</td>
+						</tr>
+						<tr>
+							<td>55</td>
+							<td>Sports duffle bag</td>
+							<td>&euro;15,00</td>
+						</tr>
+						<tr>
+							<td>23</td>
+							<td>Some stuff </td>
+							<td>&euro;489,00</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
 	</div>
 </div>
 
-<?php include 'shared/footer.php'; ?>	
+<?php include 'shared/footer.php'; ?>
