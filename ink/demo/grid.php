928ca70de34f9e3a15293cc284deff5610443281
Gonçalo Fonseca <goncalo.fonseca@telecom.pt>
2012-09-11 17:35:12 +0100

    WIP - Fixes ink grid classes bugs

--- ink/demo/grid.php
+++ ink/demo/grid.php
@@ -39,22 +39,22 @@
 		<h2>Containers</h2>
 		<p>The ink-container class is where you define the width of your layout. You should define either a static width, a relative width or a maximum width for this element and use it to contain your layout.</p>
 	</div>
-	<div class="ink-section gridExemple ink-clearfix">
+	<div class="ink-section gridExemple ink-clear">
 		<div class="ink-space box">
-			<div class="ink-l100 ink-m100 ink-s100 ink-clear"><p>100%</p></div>
-			<div class="ink-l90 ink-m90 ink-s90 ink-clear"><p>90%</p></div>
-			<div class="ink-l80 ink-m80 ink-s80 ink-clear"><p>80%</p></div>
-			<div class="ink-l75 ink-m75 ink-s75 ink-clear"><p>75%</p></div>
-			<div class="ink-l70 ink-m70 ink-s70 ink-clear"><p>70%</p></div>
-			<div class="ink-l66 ink-m66 ink-s66 ink-clear"><p>66%</p></div>
-			<div class="ink-l60 ink-m60 ink-s60 ink-clear"><p>60%</p></div>
-			<div class="ink-l50 ink-m50 ink-s50 ink-clear"><p>50%</p></div>
-			<div class="ink-l40 ink-m40 ink-s40 ink-clear"><p>40%</p></div>
-			<div class="ink-l33 ink-m33 ink-s33 ink-clear"><p>33%</p></div>
-			<div class="ink-l30 ink-m30 ink-s30 ink-clear"><p>30%</p></div>
-			<div class="ink-l25 ink-m25 ink-s25 ink-clear"><p>25%</p></div>
-			<div class="ink-l20 ink-m20 ink-s20 ink-clear"><p>20%</p></div>
-			<div class="ink-l10 ink-m10 ink-s10 ink-clear"><p>10%</p></div>
+			<div class="ink-l100 ink-m100"><p>100%</p></div>
+			<div class="ink-l90 ink-m100 ink-clear"><p>90%</p></div>
+			<div class="ink-l80 ink-m100 ink-clear"><p>80%</p></div>
+			<div class="ink-l75 ink-m100 ink-clear"><p>75%</p></div>
+			<div class="ink-l70 ink-m100 ink-clear"><p>70%</p></div>
+			<div class="ink-l66 ink-m100 ink-clear"><p>66%</p></div>
+			<div class="ink-l60 ink-m100 ink-clear"><p>60%</p></div>
+			<div class="ink-l50 ink-m100 ink-clear"><p>50%</p></div>
+			<div class="ink-l40 ink-m100 ink-clear"><p>40%</p></div>
+			<div class="ink-l33 ink-m100 ink-clear"><p>33%</p></div>
+			<div class="ink-l30 ink-m100 ink-clear"><p>30%</p></div>
+			<div class="ink-l25 ink-m100 ink-clear"><p>25%</p></div>
+			<div class="ink-l20 ink-m100 ink-clear"><p>20%</p></div>
+			<div class="ink-l10 ink-m100 ink-clear"><p>10%</p></div>
 		</div>
 
 		<div class="ink-l33 ink-clear-left">
@@ -89,15 +89,15 @@
 			</div>
 			<div class="ink-l50 gridExemple2">
 				<div class="ink-space box">
-					<div class="ink-l100 ink-clearfix level1">
+					<div class="ink-l100 ink-m100 level1">
 						<p>100%</p>
-						<div class="ink-l50 level2"><p>50%</p></div>
-						<div class="ink-l50 level2"><p>50%</p></div>
-						<div class="ink-l25 level2"><p>25%</p></div>
-						<div class="ink-l20 level2"><p>20%</p></div>
-						<div class="ink-l10 level2"><p>10%</p></div>
-						<div class="ink-l20 level2"><p>20%</p></div>
-						<div class="ink-l25 level2"><p>25%</p></div>
+						<div class="ink-l50 ink-m100 level2"><p>50%</p></div>
+						<div class="ink-l50 ink-m100 level2"><p>50%</p></div>
+						<div class="ink-l25 ink-m100 level2"><p>25%</p></div>
+						<div class="ink-l20 ink-m100 level2"><p>20%</p></div>
+						<div class="ink-l10 ink-m100 level2"><p>10%</p></div>
+						<div class="ink-l20 ink-m100 level2"><p>20%</p></div>
+						<div class="ink-l25 ink-m100 level2"><p>25%</p></div>
 					</div>
 				</div>
 				<pre  class="prettyprint">&lt;div class=&quot;ink-l100&quot;&gt;
@@ -112,21 +112,21 @@
 			</div>
 			<div class="ink-l50 gridExemple2">
 				<div class="ink-space box">
-					<div class="ink-l100 ink-clearfix level1">
-						<div class="ink-l50 level2">
-							<p>50%</p>
-							<div class="ink-l50  level2"><p>50%</p></div>
-							<div class="ink-l50  level2"><p>50%</p></div>
+					<div class="ink-l100 ink-m100 level1">
+						<div class="ink-l50 ink-m100 level2">
+							<div class="ink-l100 ink-m100 level2"><p>50%</p></div>
+							<div class="ink-l50 ink-m100 level2"><p>50%</p></div>
+							<div class="ink-l50 ink-m100 level2"><p>50%</p></div>
 						</div>
-						<div class="ink-l50 level2">
-							<p>50%</p>
-							<div class="ink-l50  level2"><p>50%</p></div>
-							<div class="ink-l50  level2"><p>50%</p></div>
+						<div class="ink-l50 ink-m100 level2">
+							<div class="ink-l100 ink-m100 level2"><p>50%</p></div>
+							<div class="ink-l50 ink-m100 level2"><p>50%</p></div>
+							<div class="ink-l50 ink-m100 level2"><p>50%</p></div>
 						</div>
-						<div class="ink-l25"><p>25%</p></div>
-						<div class="ink-l25"><p>25%</p></div>
-						<div class="ink-l25"><p>25%</p></div>
-						<div class="ink-l25"><p>25%</p></div>
+						<div class="ink-l25 ink-m100"><p>25%</p></div>
+						<div class="ink-l25 ink-m100"><p>25%</p></div>
+						<div class="ink-l25 ink-m100"><p>25%</p></div>
+						<div class="ink-l25 ink-m100"><p>25%</p></div>
 					</div>
 				</div>
 				<pre  class="prettyprint">&lt;div class=&quot;ink-l50&quot;&gt;
