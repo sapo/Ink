46306c79d5bf23b32f74d3093eada015e13c2508
Pedro Correia <pedro.correia@gmail.com>
2012-09-25 11:51:29 +0100

    InK now builds as a single css file. The large, medium and small LESS files contain their respective media query rules and are imported by ink.less to produce a single css file for the framework.

--- ink/demo/index.php
+++ ink/demo/index.php
@@ -5,15 +5,15 @@
     
 <nav id="topbar">
 	<div class="ink-container">
-		<h1><a class="logoPlaceholder" href="./" title="Site Title">InK<small>Interface kit</small></a></h1>
-		<ul>
+		<ul class="ink-h-nav">
+			<li><a class="logoPlaceholder" href="./" title="Site Title">InK</a></li>
 			<li><a href="grid.php">Layout</a></li>
 			<li><a href="navigation.php">Navigation</a></li>
 			<li><a href="typo.php">Typography & Icons</a></li>
 			<li><a href="forms.php">Forms</a></li>
 			<li><a href="alerts.php">Alerts</a></li>
 			<li><a href="tables.php">Tables</a></li>
-			<li><a href="alerts.php">InkJS</a></li>
+			<li><a href="widgets.php">InkJS</a></li>
 		</ul>
 	</div>
 </nav>  
