562b449191a98de7f1f476ae64fb0182a4c49035
Pedro Correia <pedro.correia@gmail.com>
2012-10-11 16:29:32 +0100

    Moves important site files away from mitras

--- site/tree/index.php
+++ site/tree/index.php
@@ -56,7 +56,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = 'system';
+	$system_path = '../system';
 
 /*
  *---------------------------------------------------------------
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'application';
+	$application_folder = '../application';
 
 /*
  * --------------------------------------------------------------------
