494abb53feec7e82eaa4d5539db08ba27562776f
Claudio Gamboa <gamboa@co.sapo.pt>
2012-10-12 16:38:33 +0100

    changed header... call all src from ink.dev

--- ink/demo/shared/header.php
+++ ink/demo/shared/header.php
@@ -11,12 +11,12 @@
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
 
     <link rel="shortcut icon" href="styles/imgs/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" href="http://ink.staging.sapo.pt/assets/imgs/touch-icon.57.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://ink.staging.sapo.pt/assets/imgs/touch-icon.72.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://ink.staging.sapo.pt/assets/imgs/touch-icon.114.png">
-	<link rel="apple-touch-startup-image" href="http://ink.staging.sapo.pt/assets/imgs/splash.320x460.png" media="screen and (min-device-width: 200px) and (max-device-width: 320px) and (orientation:portrait)">
-	<link rel="apple-touch-startup-image" href="http://ink.staging.sapo.pt/assets/imgs/splash.768x1004.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)">
-	<link rel="apple-touch-startup-image" href="http://ink.staging.sapo.pt/assets/imgs/splash.1024x748.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)">
+    <link rel="apple-touch-icon-precomposed" href="http://ink.dev.sapo.pt/assets/imgs/touch-icon.57.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://ink.dev.sapo.pt/assets/imgs/touch-icon.72.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://ink.dev.sapo.pt/assets/imgs/touch-icon.114.png">
+	<link rel="apple-touch-startup-image" href="http://ink.dev.sapo.pt/assets/imgs/splash.320x460.png" media="screen and (min-device-width: 200px) and (max-device-width: 320px) and (orientation:portrait)">
+	<link rel="apple-touch-startup-image" href="http://ink.dev.sapo.pt/assets/imgs/splash.768x1004.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)">
+	<link rel="apple-touch-startup-image" href="http://ink.dev.sapo.pt/assets/imgs/splash.1024x748.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)">
 	
 	<link rel="stylesheet" href="http://ink.dev.sapo.pt/assets/css/ink.css" type="text/css" media="screen" title="no title">
 	<link rel="stylesheet" href="http://ink.dev.sapo.pt/assets/css/demo.css" type="text/css" media="screen" title="no title">
@@ -38,10 +38,10 @@
 	<link rel="stylesheet" href="http://ink.dev.sapo.pt/assets/css/font-awesome-ie7.css">
 	<![endif]-->
 	
-	<script type="text/javascript" src="http://ink.staging.sapo.pt/assets/js/html5shiv.js"></script>
-	<script type="text/javascript" src="http://ink.staging.sapo.pt/assets/js/respond.src.js"></script>
+	<script type="text/javascript" src="http://ink.dev.sapo.pt/assets/js/html5shiv.js"></script>
+	<script type="text/javascript" src="http://ink.dev.sapo.pt/assets/js/respond.src.js"></script>
 
-    <script type="text/javascript" src="http://js.staging.sapo.pt/Bundles/Ink.js"></script>
+    <script type="text/javascript" src="http://js.dev.sapo.pt/Bundles/Ink.js"></script>
 	
 </head>
 <body>
