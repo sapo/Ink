16bdc78bb31e9f9f4dbaec4f25e9020a5182cbb5
Pedro Correia <pedro.correia@gmail.com>
2012-10-18 18:04:24 +0100

    Some more navigation tweaks

--- site/application/views/navigation.php
+++ site/application/views/navigation.php
@@ -5,14 +5,16 @@
 	</div>
 </div>
 
-<nav class="menu">
+<nav class="menu-second-level">
 	<div class="ink-container">
-		<ul class="ink-h-nav">
-			<li class="active"><a class="home" href="#">Home</a></li>
-			<li><a href="#">Horizontal menu</a></li>
-			<li><a href="#">Vertical menu</a></li>
-			<li><a href="#">Simple footer menu</a></li>
-		</ul>
+		<nav class="ink-navigation">
+			<ul class="menu horizontal">
+				<li class="active"><a class="home" href="#">Home</a></li>
+				<li><a href="#">Horizontal menu</a></li>
+				<li><a href="#">Vertical menu</a></li>
+				<li><a href="#">Simple footer menu</a></li>
+			</ul>
+		</nav>
 	</div>
 </nav>  
 
@@ -21,13 +23,98 @@
 		<div class="ink-vspace">
 			<h3>Horizontal menu</h3>
 			<p>
-				Horizontal menus can be used as primary navigation of the website or applied as top bar.
-				To build this navigation you just need to use the class <code>.ink-h-nav</code>.
+				To build a horizontal menu with ink start with a block-level element and use the <code>ink-navigation</code> class. We prefer to use a <code><?php echo htmlentities('<nav>') ?></code> tag to wrap our navigation items, but a <code><?php echo htmlentities('<div>') ?></code> will do just fine.
 			</p>
+			<p>The menu itself is built with a unordered list with the <code>menu</code> and <code>horizontal</code> classes. Adding color is as simple as adding a class to the menu list element. Available classes are <code>grey</code>, <code>green</code>, <code>blue</code>, <code>red</code>, <code>orange</code> and <code>black</code>!</p>
 		</div>
 		
-		<nav>
-			<ul class="ink-h-nav example_menu">
+		<nav class="ink-navigation">
+			<ul class="menu horizontal grey separated">
+				<li class="active"><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li>
+					<a href="#">Submenu<i class="icon-caret-down"></i></a>
+					<ul class="submenu">
+						<li><a href="#">Item with a very big title</a></li>
+						<li><a href="#">Item</a></li>
+						<li><a href="#">Item</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+
+		<nav class="ink-navigation ink-vspace">
+			<ul class="menu horizontal green separated">
+				<li class="active"><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li>
+					<a href="#">Submenu<i class="icon-caret-down"></i></a>
+					<ul class="submenu">
+						<li><a href="#">Item with a very big title</a></li>
+						<li><a href="#">Item</a></li>
+						<li><a href="#">Item</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+
+		<nav class="ink-navigation ink-vspace">
+			<ul class="menu horizontal blue">
+				<li class="active"><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li>
+					<a href="#">Submenu<i class="icon-caret-down"></i></a>
+					<ul class="submenu">
+						<li><a href="#">Item with a very big title</a></li>
+						<li><a href="#">Item</a></li>
+						<li><a href="#">Item</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+
+		<nav class="ink-navigation ink-vspace">
+			<ul class="menu horizontal red">
+				<li class="active"><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li>
+					<a href="#">Submenu<i class="icon-caret-down"></i></a>
+					<ul class="submenu">
+						<li><a href="#">Item with a very big title</a></li>
+						<li><a href="#">Item</a></li>
+						<li><a href="#">Item</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+
+		<nav class="ink-navigation ink-vspace">
+			<ul class="menu horizontal orange">
+				<li class="active"><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li><a href="#">Item</a></li>
+				<li>
+					<a href="#">Submenu<i class="icon-caret-down"></i></a>
+					<ul class="submenu">
+						<li><a href="#">Item with a very big title</a></li>
+						<li><a href="#">Item</a></li>
+						<li><a href="#">Item</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+
+		<nav class="ink-navigation ink-vspace">
+			<ul class="menu horizontal black">
 				<li class="active"><a href="#">Item</a></li>
 				<li><a href="#">Item</a></li>
 				<li><a href="#">Item</a></li>
@@ -50,8 +137,8 @@
 					<code>&lt;ul&gt;</code> or the new <span class="label_new">HTML5</span> tag <code>&lt;nav&gt;</code>
 				</p>
 <pre class="prettyprint linenums">
-<?php echo(htmlentities('<nav>
-  <ul class="ink-h-nav">
+<?php echo(htmlentities('<nav class="ink-navigation">
+  <ul class="menu horizontal grey">
     <li><a href="#">Item</a></li>
     <li><a href="#">Item</a></li>
     <li><a href="#">Item</a></li>
@@ -65,8 +152,8 @@
 				<h4>Submenu</h4>
 				<p><code>&lt;ul&gt;</code> (Unsorted Lists) based menus may have submenus respecting the following structure:</p>
 <pre class="prettyprint linenums">
-<?php echo(htmlentities('<nav>
-  <ul class="ink-h-nav">
+<?php echo(htmlentities('<nav class="ink-navigation">
+  <ul class="menu horizontal grey">
     <li><a href="#">Item</a></li>
     <li><a href="#">Item</a></li>
     <li><a href="#">Item</a></li>
@@ -91,21 +178,23 @@
 		<div class="ink-row">
 			<div class="ink-l40">
 				<div class="ink-gutter">
-					<ul class="unstyled ink-v-nav">
-						<li><a href="#">Suspendisse</a></li>
-						<li><a href="#">Vivamus</a></li>
-						<li><a href="#">Condimentum</a></li>
-						<li><a href="#">Ccommodo</a></li>
-						<li><a href="#">Egestas</a></li>
-						<li>
-							<a href="#">Submenu<i class="icon-caret-right"></i></a>
-							<ul class="submenu">
-								<li><a href="#">Item with a very big title</a></li>
-								<li><a href="#">Item</a></li>
-								<li><a href="#">Item</a></li>
-							</ul>
-						</li>
-					</ul>
+					<nav class="ink-navigation">
+						<ul class="menu vertical green">
+							<li><a href="#">Suspendisse</a></li>
+							<li><a href="#">Vivamus</a></li>
+							<li><a href="#">Condimentum</a></li>
+							<li><a href="#">Ccommodo</a></li>
+							<li><a href="#">Egestas</a></li>
+							<li>
+								<a href="#">Submenu<i class="icon-caret-right"></i></a>
+								<ul class="submenu">
+									<li><a href="#">Item with a very big title</a></li>
+									<li><a href="#">Item</a></li>
+									<li><a href="#">Item</a></li>
+								</ul>
+							</li>
+						</ul>
+					</nav>
 				</div>
 			</div>
 			<div class="ink-l60">
@@ -156,21 +245,27 @@
 			<p>
 				Paginations can be created by adding the <code>.pagination</code> class to a <code>.ink-h-nav</code> list. The <code>.active</code> and <code>.disabled</code> classes are available to diferentiate the current page and disabled links.
 			</p>
-			<div class="ink-vspace">
-				<ul class="ink-h-nav pagination">
-					<li class="disabled"><a href="#">&laquo;</a></li>
-					<li><a href="#">1</a></li>
-					<li><a href="#">2</a></li>
-					<li><a href="#">3</a></li>
-					<li class="active"><a href="#">4</a></li>
-					<li><a href="#">5</a></li>
-					<li><a href="#">6</a></li>
-					<li><a href="#">7</a></li>
-					<li><a href="#">&raquo;</a></li>
-				</ul>
+			<div class="ink-vspace visual-example">
+				<h4>Example</h4>
+				<div class="example-container">
+					<nav class="ink-navigation">
+						<ul class="pagination">
+							<li class="disabled previous"><a href="#">Previous</a></li>
+							<li><a href="#">1</a></li>
+							<li><a href="#">2</a></li>
+							<li><a href="#">3</a></li>
+							<li class="active"><a href="#">4</a></li>
+							<li><a href="#">5</a></li>
+							<li><a href="#">6</a></li>
+							<li><a href="#">7</a></li>
+							<li class="next"><a href="#">Next</a></li>
+						</ul>
+					</nav>
+				</div>
 			</div>
 <pre class="prettyprint linenums">
-<?php echo(htmlentities('<ul class="ink-h-nav pagination">
+<?php echo(htmlentities('<nav class="ink-navigation">
+   <ul class="pagination">
    <li class="disabled"><a href="#">&laquo;</a></li>
    <li><a href="#">1</a></li>
    <li><a href="#">2</a></li>
@@ -180,7 +275,7 @@
    <li><a href="#">6</a></li>
    <li><a href="#">7</a></li>
    <li><a href="#">&raquo;</a></li>
-</ul>')) ?></pre>
+</ul></nav>')) ?></pre>
 		</div>
 	</div>
 	<div class="ink-section">
@@ -189,13 +284,18 @@
 			<p>
 				Pills can be created by adding the <code>.pills</code> class to a <code>.ink-h-nav</code> list. The <code>.active</code> and <code>.disabled</code> classes are available.
 			</p>
-			<div class="ink-vspace">
-				<ul class="ink-h-nav pills">
-					<li><a href="#">Item</a></li>
-					<li class="active"><a href="#">Item</a></li>
-					<li><a href="#">Item</a></li>
-					<li><a href="#">Item</a></li>
-				</ul>
+			<div class="ink-vspace visual-example">
+				<h4>example</h4>
+				<div class="example-container">
+					<nav class="ink-navigation">
+						<ul class="pills">
+							<li><a href="#">Item</a></li>
+							<li class="active"><a href="#">Item</a></li>
+							<li><a href="#">Item</a></li>
+							<li><a href="#">Item</a></li>
+						</ul>
+					</nav>
+				</div>
 			</div>
 <pre class="prettyprint linenums">
 <?php echo(htmlentities('<ul class="ink-h-nav pills">
@@ -212,12 +312,14 @@
 				Breadcrumbs can be created by adding the <code>.breadcrumbs</code> class to a <code>.ink-h-nav</code> list. The <code>.active</code> and <code>.disabled</code> classes are available.
 			</p>
 		<div class="ink-vspace">
-			<ul class="ink-h-nav breadcrumbs">
-				<li><a href="#">Home</a><span class="separator">/</span></li>
-				<li><a href="#">Products</a><span class="separator">/</span></li>
-				<li><a href="#">Category</a><span class="separator">/</span></li>
-				<li class="active"><a href="#">Current item</a></li>
-			</ul>
+			<nav class="ink-navigation">
+				<ul class="breadcrumbs">
+					<li><a href="#">Home</a><span class="separator">/</span></li>
+					<li><a href="#">Products</a><span class="separator">/</span></li>
+					<li><a href="#">Category</a><span class="separator">/</span></li>
+					<li class="active"><a href="#">Current item</a></li>
+				</ul>
+			</nav>
 <pre class="prettyprint linenums">
 <?php echo(htmlentities('<ul class="ink-h-nav breadcrumbs">
    <li><a href="#">Home</a><span class="separator">/</span></li>
