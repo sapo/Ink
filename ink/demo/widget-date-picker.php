aed21fcd6694f49b895a14e94bbf88f03e53d28b
Gonçalo Fonseca <goncalo.fonseca@telecom.pt>
2012-10-23 15:47:11 +0100

    Adds style to datepicker component

--- ink/demo/widget-date-picker.php
+++ ink/demo/widget-date-picker.php
@@ -1,28 +1,27 @@
 <?php include 'shared/header.php'; ?>
 
-<link rel="stylesheet" type="text/css" href="http://js.staging.sapo.pt/Assets/Images/DatePicker/style.css"/>
-
-<div class="ink-l70">   
-    <div class="ink-space">
-
-        <h3>default (on input text focus)</h3>
-        <p>
-            <input id="x" type="text" value=""/>
-        </p>
-
-
-
-        <h3>mode displayInSelect</h3>
-        <p>
-            <select id="y"></select>
-            <select id="m"></select>
-            <select id="d"></select>
-            <a href="#" id="pick">open</a>
-        </p>
-
-    </div>
+<div class="ink-container">
+	<div class="ink-l60">   
+		<div class="ink-space">
+			<form class="ink-form-block">
+				<fieldset>
+					<legend><h4>default (on input text focus)</h4></legend>
+					<div class="ink-form-wrapper">
+						<input id="x" type="text" value=""/>
+					</div>
+	
+					<legend><h4>mode displayInSelect</h4></legend>
+					<div class="ink-form-wrapper">
+						<select class="ink-mini-input" id="y"></select>
+						<select class="ink-mini-input" id="m"></select>
+						<select class="ink-mini-input" id="d"></select>
+						<a href="#" id="pick">open</a>
+					</div>
+				</fieldset>
+			</form>
+		</div>
+	</div>
 </div>
-
 
 <script type="text/javascript">
     var dp1 = new SAPO.Ink.DatePicker('#x');
