1b3ffb83327df0ccef0692952353fb689e35a866
Claudio Gamboa <gamboa@co.sapo.pt>
2012-10-24 15:59:50 +0100

    added encrypt key in configs

--- site/application/config/staging/config.php
+++ site/application/config/staging/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = sha1('ptSAPOiNk@!!---2134567774567927*');
 
 /*
 |--------------------------------------------------------------------------
