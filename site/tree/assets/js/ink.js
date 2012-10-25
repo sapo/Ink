8825840314bc86118790550b4f61cd0b03c1498a
Ricardo Machado <ricardo-s-machado@telecom.pt>
2012-10-25 15:14:33 +0100

    Changelog: Added the onShow option when creating the color picker

--- site/tree/assets/js/ink.js
+++ site/tree/assets/js/ink.js
@@ -16,7 +16,7 @@ SAPO.Dom.Event.observe(document, "dom:loaded", function(){
             var modal = new SAPO.Ink.Modal( undefined, {
                 'width': 300,
                 'height': 500,
-                'markup': '<div class="ink-space"><div id="colorwheel" style="width:218px;margin:0 auto;" class="ib"></div><div class="ib"><p><label for="hexColor">hex color</label><input type="text" id="hexColor" value="#ff0000" /></p><p><label>sample</label><div id="sample" style="position:relaative;margin:0 auto;height:50px;width:50px;"></div></p></div><div style="width:218px;margin;0 auto;text-align:center"><button class="ink-button success">Done</button><button class="ink-button">Cancel</button></div></div>'
+                'markup': '<div class="ink-space"><div id="colorwheel" style="width:218px;margin:0 auto;" class="ib"></div><div class="ib"><p><label for="hexColor">hex color</label><input type="text" id="hexColor" value="#ff0000" /></p><p><label>sample</label><div id="sample" style="position:relative;margin:0 auto;height:50px;width:50px;"></div></p></div><div style="width:218px;margin;0 auto;text-align:center"><button class="ink-button success">Done</button><button class="ink-button">Cancel</button></div></div>'
             });
 
             SAPO.Dom.Event.observe( SAPO.Dom.Selector.select('.ink-modal .ink-button.success')[0], 'click', function(event){
@@ -41,14 +41,18 @@ SAPO.Dom.Event.observe(document, "dom:loaded", function(){
                     s$('hexColor').style.color = ( cw._value>0.5 ) ? '#000' : '#FFF';
                     s$('hexColor').style.backgroundColor = hex ;
                     s$('sample').style.backgroundColor = hex;
+                },
+                onShow: function( cw ){
+                    cw.setColor(self.value || '#000000');
                 }
             });
 
             SAPO.Dom.Event.observe('hexColor', 'change', function() {
                 var hex = s$('hexColor').value;
-                s$('sample').style.backgroundColor = hex;
                 cw.setColor(hex);
-                s$('hexColor').style.backgroundColor = ( cw._value>0.5 ) ? '#000' : '#FFF' ;
+                s$('sample').style.backgroundColor = hex;
+                s$('hexColor').style.color = ( cw._value>0.5 ) ? '#000' : '#FFF' ;
+                s$('hexColor').style.backgroundColor = hex ;
             });
         });
     }
