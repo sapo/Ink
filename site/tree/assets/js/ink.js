3d0f52b700397804d4124e67bd9979cddbeeb274
Ricardo Machado <ricardo-s-machado@telecom.pt>
2012-10-25 18:11:41 +0100

    Changelog: Implemented changes to check if the elements exist before using them. Also implemented some variables to re-use (instead of re-select them in the DOM).

--- site/tree/assets/js/ink.js
+++ site/tree/assets/js/ink.js
@@ -8,7 +8,6 @@ SAPO.Dom.Event.observe(document, "dom:loaded", function(){
 
     for( i=0; i<colorPickers.length; i++ )
     {
-
         SAPO.Dom.Event.observe(colorPickers[i],'click',function(event){
 
             var self = this;
@@ -19,41 +18,54 @@ SAPO.Dom.Event.observe(document, "dom:loaded", function(){
                 'markup': '<div class="ink-space"><div id="colorwheel" style="width:218px;margin:0 auto;" class="ib"></div><div class="ib"><p><label for="hexColor">hex color</label><input type="text" id="hexColor" value="#ff0000" /></p><p><label>sample</label><div id="sample" style="position:relative;margin:0 auto;height:50px;width:50px;"></div></p></div><div style="width:218px;margin;0 auto;text-align:center"><button class="ink-button success">Done</button><button class="ink-button">Cancel</button></div></div>'
             });
 
-            SAPO.Dom.Event.observe( SAPO.Dom.Selector.select('.ink-modal .ink-button.success')[0], 'click', function(event){
-                self.value = s$('hexColor').value;
-                self.style.color = s$('hexColor').style.color;
-                self.style.backgroundColor = s$('hexColor').value;
-                modal.dismiss();
-            });
+            if( modal ){
+                var element = SAPO.Dom.Selector.select('.ink-modal .ink-button');
+                var hexColor = s$('hexColor');
+                var sample = s$('sample');
 
-            SAPO.Dom.Event.observe( SAPO.Dom.Selector.select('.ink-modal .ink-button')[1], 'click', function(event){
-                modal.dismiss();
-            });
-
-
-
-            var cw = new SAPO.Component.ColorWheel({
-                container:  'colorwheel',
-                cssURI: 'http://js.sapo.pt/Assets/Images/ComponentColorWheel/style.css',
-                onChange:   function(cw) {
-                    var hex = cw.getColorHex();
-                    s$('hexColor').value = hex;
-                    s$('hexColor').style.color = ( cw._value>0.5 ) ? '#000' : '#FFF';
-                    s$('hexColor').style.backgroundColor = hex ;
-                    s$('sample').style.backgroundColor = hex;
-                },
-                onShow: function( cw ){
-                    cw.setColor(self.value || '#000000');
+                if( element.length > 0 ){
+                    SAPO.Dom.Event.observe( element[0], 'click', function(event){
+                        if( hexColor ){
+                            self.value = hexColor.value;
+                            self.style.color = hexColor.style.color;
+                            self.style.backgroundColor = hexColor.value;
+                            modal.dismiss();
+                        }
+                    });
                 }
-            });
 
-            SAPO.Dom.Event.observe('hexColor', 'change', function() {
-                var hex = s$('hexColor').value;
-                cw.setColor(hex);
-                s$('sample').style.backgroundColor = hex;
-                s$('hexColor').style.color = ( cw._value>0.5 ) ? '#000' : '#FFF' ;
-                s$('hexColor').style.backgroundColor = hex ;
-            });
+                SAPO.Dom.Event.observe( element[1], 'click', function(event){
+                    modal.dismiss();
+                });
+
+                var cw = new SAPO.Component.ColorWheel({
+                    container:  'colorwheel',
+                    cssURI: 'http://js.sapo.pt/Assets/Images/ComponentColorWheel/style.css',
+                    onChange:   function(cw) {
+                        var hex = cw.getColorHex();
+                        if( hexColor ){
+                            hexColor.value = hex;
+                            hexColor.style.color = ( cw._value>0.5 ) ? '#000' : '#FFF';
+                            hexColor.style.backgroundColor = hex ;
+                            sample.style.backgroundColor = hex;
+                        }
+                    },
+                    onShow: function( cw ){
+                        cw.setColor(self.value || '#000000');
+
+                        SAPO.Dom.Event.observe('hexColor', 'change', function() {
+                            if( hexColor )
+                            {
+                                var hex = hexColor.value;
+                                cw.setColor(hex);
+                                sample.style.backgroundColor = hex;
+                                hexColor.style.color = ( cw._value>0.5 ) ? '#000' : '#FFF' ;
+                                hexColor.style.backgroundColor = hex ;
+                            }
+                        });
+                    }
+                });
+            }
         });
     }
 }());
