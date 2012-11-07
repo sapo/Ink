e1024285ac506248f57c5c50360427c79f0fcec5
Ricardo Machado <ricardo-s-machado@telecom.pt>
2012-11-07 19:05:22 +0000

    Forcing ['SERVER_NAME'] on the base_url

--- site/application/config/staging/config.php
+++ site/application/config/staging/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://ink.staging.sapo.pt/';
+$config['base_url']	= 'http://' . $_SERVER['SERVER_NAME'] . '/';
 
 /*
 |--------------------------------------------------------------------------
