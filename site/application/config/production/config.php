2713dcc14c375a686e8c8af37d5fa00ef7bfcc6a
Claudio Gamboa <gamboa@co.sapo.pt>
2012-11-08 13:44:44 +0000

    changed path

--- site/application/config/production/config.php
+++ site/application/config/production/config.php
@@ -368,7 +368,7 @@ $config['proxy_ips'] = '';
 */
 $config['base_path']    = BASEPATH . '../';
 $config['build_path']   = '/fasmounts/sapo/VAR/ink/build/';
-$config['latest_path']  = BASEPATH . '../../ink/';
+$config['latest_path']  = BASEPATH . '../ink/';
 
 $config['build_normal_css_url'] = 'http://sgt-ink-be-vip.singletons.bk.sapo.pt:8081/getcss';
 $config['build_minimized_css_url']  = 'http://sgt-ink-be-vip.singletons.bk.sapo.pt:8081/getcss';
