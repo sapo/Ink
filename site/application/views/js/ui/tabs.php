8e72367e06592ab20c73010c2536c9630573da6f
Gonçalo Fonseca <goncalo.fonseca@telecom.pt>
2012-11-13 17:37:53 +0000

    Normalizes ink-tabs markup.

--- site/application/views/js/ui/tabs.php
+++ site/application/views/js/ui/tabs.php
@@ -62,87 +62,84 @@ JS;
 
             <div class="ink-l70">
                 <div class="ink-gutter">
-                    <div class="ink-row box">
-                        <div class="ink-gutter">
-                            <div class="ink-tabs">
-                                <nav class="ink-navigation">
-                                    <ul class="ink-tabs-nav menu horizontal">
-                                        <li class="active"><a href="#home">Home</a></li>
-                                        <li><a href="#news">News</a></li>
-                                        <li><a href="#description">Description</a></li>
-                                        <li><a href="#stuff">Stuff</a></li>
-                                        <li><a href="#more_stuff">More stuff</a></li>
-                                    </ul>
-                                </nav>
-                                <div id="home" class="ink-tabs-container">
-                                    <p>
-                                        Arnold ipsum. Well then God shouldn't have cloned my dog. I'm a cybernetic organizm. Living tissue over endoskeleton. 
-                                        You're not going to have your mommies right behind you to wipe your little tushies. Blondes. Consider it a divorce. 
-                                        Talk to the hand. Take it BACK. I'll be back. Knock knock. I did nothing. The pavement with his enemy. You LIE! 
-                                        I'm not shitting on you. Get down or I'll put you down. You are not you you're me. Scumbag. Bring your toy back to the carpet. 
-                                        Bring it back to the carpet. Dylan. You son of a bitch. Get down. Of course. I'm a Terminator. 
-                                        Talk to the hand. I need your clothes, your boots, and your motorcycle. Into the tunnel. My name is John Kimble and I love my car. I'll be back.
-                                    </p>
-                                </div>
-                                <div id="news" class="ink-tabs-container" style="display:none">
-                                    <p>
-                                        Arnold ipsum. Well then God shouldn't have cloned my dog. I'm a cybernetic organizm. Living tissue over endoskeleton. 
-                                        You're not going to have your mommies right behind you to wipe your little tushies. Blondes. Consider it a divorce. 
-                                        Talk to the hand. Take it BACK. I'll be back. Knock knock. I did nothing. The pavement with his enemy. You LIE! 
-                                        I'm not shitting on you. Get down or I'll put you down. You are not you you're me. Scumbag. Bring your toy back to the carpet. 
-                                        Bring it back to the carpet. Dylan. You son of a bitch. Get down. Of course. I'm a Terminator. 
-                                        Talk to the hand. I need your clothes, your boots, and your motorcycle. Into the tunnel. My name is John Kimble and I love my car. I'll be back.
-                                    </p>
-                                </div>
-                                <div id="descripton" class="ink-tabs-container" style="display:none">
-                                    <p>
-                                        Arnold ipsum. Well then God shouldn't have cloned my dog. I'm a cybernetic organizm. Living tissue over endoskeleton. 
-                                        You're not going to have your mommies right behind you to wipe your little tushies. Blondes. Consider it a divorce. 
-                                        Talk to the hand. Take it BACK. I'll be back. Knock knock. I did nothing. The pavement with his enemy. You LIE! 
-                                        I'm not shitting on you. Get down or I'll put you down. You are not you you're me. Scumbag. Bring your toy back to the carpet. 
-                                        Bring it back to the carpet. Dylan. You son of a bitch. Get down. Of course. I'm a Terminator. 
-                                        Talk to the hand. I need your clothes, your boots, and your motorcycle. Into the tunnel. My name is John Kimble and I love my car. I'll be back.
-                                    </p>
-                                </div>
-                                <div id="stuff" class="ink-tabs-container" style="display:none">
-                                    <p>
-                                        Arnold ipsum. Well then God shouldn't have cloned my dog. I'm a cybernetic organizm. Living tissue over endoskeleton. 
-                                        You're not going to have your mommies right behind you to wipe your little tushies. Blondes. Consider it a divorce. 
-                                        Talk to the hand. Take it BACK. I'll be back. Knock knock. I did nothing. The pavement with his enemy. You LIE! 
-                                        I'm not shitting on you. Get down or I'll put you down. You are not you you're me. Scumbag. Bring your toy back to the carpet. 
-                                        Bring it back to the carpet. Dylan. You son of a bitch. Get down. Of course. I'm a Terminator. 
-                                        Talk to the hand. I need your clothes, your boots, and your motorcycle. Into the tunnel. My name is John Kimble and I love my car. I'll be back.
-                                    </p>
-                                </div>
-                                <div id="more_stuff" class="ink-tabs-container" style="display:none">
-                                    <p>
-                                        Arnold ipsum. Well then God shouldn't have cloned my dog. I'm a cybernetic organizm. Living tissue over endoskeleton. 
-                                        You're not going to have your mommies right behind you to wipe your little tushies. Blondes. Consider it a divorce. 
-                                        Talk to the hand. Take it BACK. I'll be back. Knock knock. I did nothing. The pavement with his enemy. You LIE! 
-                                        I'm not shitting on you. Get down or I'll put you down. You are not you you're me. Scumbag. Bring your toy back to the carpet. 
-                                        Bring it back to the carpet. Dylan. You son of a bitch. Get down. Of course. I'm a Terminator. 
-                                        Talk to the hand. I need your clothes, your boots, and your motorcycle. Into the tunnel. My name is John Kimble and I love my car. I'll be back.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <br/>
-                        <a href="#" data-target="tabs_sourcecode" class="toggleTrigger">View Source Code</a>
-                        <pre id="tabs_sourcecode" style="display:none" class="prettyprint linenums ink-vspace"><?php echo(htmlentities( $js )); ?></pre>
-                    </div>
-                </div> <!-- row -->
+                    <div class="box">
+						<div class="ink-tabs">
+							<nav class="ink-navigation">
+								<ul class="ink-tabs-nav menu horizontal">
+									<li class="active"><a href="#home">Home</a></li>
+									<li><a href="#news">News</a></li>
+									<li><a href="#description">Description</a></li>
+									<li><a href="#stuff">Stuff</a></li>
+									<li><a href="#more_stuff">More stuff</a></li>
+								</ul>
+							</nav>
+							<div id="home" class="ink-tabs-container">
+								<p>
+									Arnold ipsum. Well then God shouldn't have cloned my dog. I'm a cybernetic organizm. Living tissue over endoskeleton. 
+									You're not going to have your mommies right behind you to wipe your little tushies. Blondes. Consider it a divorce. 
+									Talk to the hand. Take it BACK. I'll be back. Knock knock. I did nothing. The pavement with his enemy. You LIE! 
+									I'm not shitting on you. Get down or I'll put you down. You are not you you're me. Scumbag. Bring your toy back to the carpet. 
+									Bring it back to the carpet. Dylan. You son of a bitch. Get down. Of course. I'm a Terminator. 
+									Talk to the hand. I need your clothes, your boots, and your motorcycle. Into the tunnel. My name is John Kimble and I love my car. I'll be back.
+								</p>
+							</div>
+							<div id="news" class="ink-tabs-container" style="display:none">
+								<p>
+									Arnold ipsum. Well then God shouldn't have cloned my dog. I'm a cybernetic organizm. Living tissue over endoskeleton. 
+									You're not going to have your mommies right behind you to wipe your little tushies. Blondes. Consider it a divorce. 
+									Talk to the hand. Take it BACK. I'll be back. Knock knock. I did nothing. The pavement with his enemy. You LIE! 
+									I'm not shitting on you. Get down or I'll put you down. You are not you you're me. Scumbag. Bring your toy back to the carpet. 
+									Bring it back to the carpet. Dylan. You son of a bitch. Get down. Of course. I'm a Terminator. 
+									Talk to the hand. I need your clothes, your boots, and your motorcycle. Into the tunnel. My name is John Kimble and I love my car. I'll be back.
+								</p>
+							</div>
+							<div id="descripton" class="ink-tabs-container" style="display:none">
+								<p>
+									Arnold ipsum. Well then God shouldn't have cloned my dog. I'm a cybernetic organizm. Living tissue over endoskeleton. 
+									You're not going to have your mommies right behind you to wipe your little tushies. Blondes. Consider it a divorce. 
+									Talk to the hand. Take it BACK. I'll be back. Knock knock. I did nothing. The pavement with his enemy. You LIE! 
+									I'm not shitting on you. Get down or I'll put you down. You are not you you're me. Scumbag. Bring your toy back to the carpet. 
+									Bring it back to the carpet. Dylan. You son of a bitch. Get down. Of course. I'm a Terminator. 
+									Talk to the hand. I need your clothes, your boots, and your motorcycle. Into the tunnel. My name is John Kimble and I love my car. I'll be back.
+								</p>
+							</div>
+							<div id="stuff" class="ink-tabs-container" style="display:none">
+								<p>
+									Arnold ipsum. Well then God shouldn't have cloned my dog. I'm a cybernetic organizm. Living tissue over endoskeleton. 
+									You're not going to have your mommies right behind you to wipe your little tushies. Blondes. Consider it a divorce. 
+									Talk to the hand. Take it BACK. I'll be back. Knock knock. I did nothing. The pavement with his enemy. You LIE! 
+									I'm not shitting on you. Get down or I'll put you down. You are not you you're me. Scumbag. Bring your toy back to the carpet. 
+									Bring it back to the carpet. Dylan. You son of a bitch. Get down. Of course. I'm a Terminator. 
+									Talk to the hand. I need your clothes, your boots, and your motorcycle. Into the tunnel. My name is John Kimble and I love my car. I'll be back.
+								</p>
+							</div>
+							<div id="more_stuff" class="ink-tabs-container" style="display:none">
+								<p>
+									Arnold ipsum. Well then God shouldn't have cloned my dog. I'm a cybernetic organizm. Living tissue over endoskeleton. 
+									You're not going to have your mommies right behind you to wipe your little tushies. Blondes. Consider it a divorce. 
+									Talk to the hand. Take it BACK. I'll be back. Knock knock. I did nothing. The pavement with his enemy. You LIE! 
+									I'm not shitting on you. Get down or I'll put you down. You are not you you're me. Scumbag. Bring your toy back to the carpet. 
+									Bring it back to the carpet. Dylan. You son of a bitch. Get down. Of course. I'm a Terminator. 
+									Talk to the hand. I need your clothes, your boots, and your motorcycle. Into the tunnel. My name is John Kimble and I love my car. I'll be back.
+								</p>
+							</div>
+						</div>
+					</div>
+					<a href="#" data-target="tabs_sourcecode" class="ink-button toggleTrigger">View Source Code</a>
+					<pre id="tabs_sourcecode" style="display:none" class="prettyprint linenums"><?php echo(htmlentities( $js )); ?></pre>
+				</div>
             </div>
         </div>
+			<script type="text/javascript">
+			var tabs = new SAPO.Ink.Tabs('.ink-tabs', {
+				disabled: ['#stuff', '#more_stuff'], 
+				active: '#news',
+				onBeforeChange: function(tab){
+					console.log('beforeChange', tab);
+				}, 
+				onChange: function(tab){
+					console.log('Changed', tab);
+				}
+			});
+	    </script>
     </div>
-    <script type="text/javascript">
-        var tabs = new SAPO.Ink.Tabs('.ink-tabs', {
-            disabled: ['#stuff', '#more_stuff'], 
-            active: '#news',
-            onBeforeChange: function(tab){
-                console.log('beforeChange', tab);
-            }, 
-            onChange: function(tab){
-                console.log('Changed', tab);
-            }
-        });
-    </script>
