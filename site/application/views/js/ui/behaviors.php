bd619480817e60be0c4bd96fd7b8e57e1a7e4d7a
Jonatas Miguel <jonatas.df.miguel@gmail.com>
2012-11-14 19:05:33 +0000

    added some new ui stuffs

---
+++ site/application/views/js/ui/behaviors.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Separated markup to be repeated in the different tabs
+ * @var [type]
+ */
+// dockable
+// collapsible
+// close
+$dockable_html = <<<HTML
+<div class="ink-container">
+    <nav class="ink-navigation ink-dockable">
+        <ul class="menu horizontal blue ink-l100 ink-m100 ink-s100">
+            <li class="active"><a class="home" href="#ui_home">Home</a></li>
+            <li><a href="#gallery">Gallery</a></li>
+            <li><a href="#modal">Modal</a></li>
+            <li><a href="#table">Table</a></li>
+            <li><a href="#tree_view">Tree View</a></li>
+            <li><a href="#sortable_list">Sortable List</a></li>
+            <li><a href="#date_picker">Date Picker</a></li>
+            <li><a href="#tabs">Tabs</a></li>
+            <li><a href="#formvalidator">Form Validator</a></li>
+        </ul>
+    </nav>
+</div>
+HTML;
+$collapsible_html = <<<HTML
+<div class="ink-container">
+    <nav class="ink-navigation ink-dockable">
+        <ul class="menu horizontal blue ink-l100 ink-m100 ink-s100">
+            <li class="active"><a class="home" href="#ui_home">Home</a></li>
+            <li><a href="#gallery">Gallery</a></li>
+            <li><a href="#modal">Modal</a></li>
+            <li><a href="#table">Table</a></li>
+            <li><a href="#tree_view">Tree View</a></li>
+            <li><a href="#sortable_list">Sortable List</a></li>
+            <li><a href="#date_picker">Date Picker</a></li>
+            <li><a href="#tabs">Tabs</a></li>
+            <li><a href="#formvalidator">Form Validator</a></li>
+        </ul>
+    </nav>
+</div>
+HTML;
+$close_html = <<<HTML
+<div class="ink-container">
+    <nav class="ink-navigation ink-dockable">
+        <ul class="menu horizontal blue ink-l100 ink-m100 ink-s100">
+            <li class="active"><a class="home" href="#ui_home">Home</a></li>
+            <li><a href="#gallery">Gallery</a></li>
+            <li><a href="#modal">Modal</a></li>
+            <li><a href="#table">Table</a></li>
+            <li><a href="#tree_view">Tree View</a></li>
+            <li><a href="#sortable_list">Sortable List</a></li>
+            <li><a href="#date_picker">Date Picker</a></li>
+            <li><a href="#tabs">Tabs</a></li>
+            <li><a href="#formvalidator">Form Validator</a></li>
+        </ul>
+    </nav>
+</div>
+HTML;
+
+/**
+ * Different vars for different layouts
+ */
+$dockable_js = <<<JS
+<script type="text/javascript">
+    var dockable = new SAPO.Ink.Dockable('.ink-dockable');
+</script>
+JS;
+$collapsible_js = <<<JS
+<script type="text/javascript">
+    var dockable = new SAPO.Ink.Dockable('.ink-dockable');
+</script>
+JS;
+$close_js = <<<JS
+<script type="text/javascript">
+    var dockable = new SAPO.Ink.Dockable('.ink-dockable');
+</script>
+JS;
+?>
+<div class="ink-section" id="behaviors">
+    <div class="ink-row ink-vspace">
+            <div class="ink-gutter">
+        <div class="ink-l90">
+                <h3>Behaviors</h3>
+        </div>
+        <div class="ink-l33">
+                <h4>Dockable</h4>
+                <p>
+                    <i>Dockable</i> keeps your menus visible while scrolling through the page.
+                </p>
+                <p>
+                    Activate it by using the "ink-dockable" class. A working example of dockable is the submenu that keeps following your progress on this page.
+                </p>
+                <p>
+                    Want to know more?
+                </p>
+                <p>
+                    Check the <a target="_blank" href="http://js.sapo.pt/SAPO/Ink/Dockable/doc.html">technical documentation</a>.</p>
+                </p>
+        </div>
+
+        <div class="ink-l33">
+                <h4>Collapsible</h4>
+                <p>
+                    <i>Collapsible</i> makes your horizontal menus vertical and collapsible depending on the screen width.
+                </p>
+                <p>
+                    Activate it by using the "ink-collapsible" class. A working example of collapsible is the top most menu.
+                </p>
+                <p>
+                    Want to know more?
+                </p>
+                <p>
+                    Check the <a target="_blank" href="http://js.sapo.pt/SAPO/Ink/Collapsible/doc.html">technical documentation</a>.</p>
+                </p>
+        </div>
+
+        <div class="ink-l33">
+                <h4>Close</h4>
+                <p>
+                    Use <i>Close</i> in conjunction with "ink-alert" or "ink-alert-block". Doing so will allow you to hide these elements from view.
+                </p>
+                <p>
+                    Activate it by using the "ink-close" class inside of an element with one of the previously mentioned classes.
+                </p>
+                <p>
+                    Want to know more?
+                </p>
+                <p>
+                    Check the <a target="_blank" href="http://js.sapo.pt/SAPO/Ink/doc.html#Close">technical documentation</a>.</p>
+                </p>
+        </div>
+    </div>
+
+    </div>
+</div>
