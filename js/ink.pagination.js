6cb523d5cfcb48ba2734dcd50cd9e24974f9cdcc
Ricardo Machado <ricardomachado@inovalabs.pt>
2013-04-18 19:04:17 +0100

    Appended some fixes done by José Dias to make the pagination behave in a more flexible way

--- js/ink.pagination.js
+++ js/ink.pagination.js
@@ -39,17 +39,26 @@
      * @constructor SAPO.Ink.Pagination.?
      * @param {String|DOMElement} selector
      * @param {Object}            options
-     * @... {Number}            size          number of pages
-     * @... {optional String}   previousLabel label to display on previous page button
-     * @... {optional String}   nextLabel     label to display on next page button
-     * @... {optional Function} onChange      optional callback
-     * @... {optional Boolean}  setHash       if true, sets hashParameter on the location.hash. default is disabled
-     * @... {optional String}   hashParameter parameter to use on setHash. by default uses 'page'
+     * @... {Number}            size              number of pages
+     * @... {optional Number}   maxSize           if passed, only shows at most maxSize items. displays also first|prev page and next page|last buttons
+     * @... {optional Number}   start             start page. defaults to 1
+     * @... {optional String}   previousLabel     label to display on previous page button
+     * @... {optional String}   nextLabel         label to display on next page button
+     * @... {optional String}   previousPageLabel label to display on previous page button
+     * @... {optional String}   nextPageLabel     label to display on next page button
+     * @... {optional String}   firstLabel        label to display on previous page button
+     * @... {optional String}   lastLabel         label to display on next page button
+     * @... {optional Function} onChange          optional callback
+     * @... {optional Boolean}  setHash           if true, sets hashParameter on the location.hash. default is disabled
+     * @... {optional String}   hashParameter     parameter to use on setHash. by default uses 'page'
      */
     var Pagination = function(selector, options) {
 
         this._options = SAPO.extendObj({
             size:          undefined,
+            start:         1,
+            firstLabel:    'First',
+            lastLabel:     'Last',
             previousLabel: 'Previous',
             nextLabel:     'Next',
             onChange:      undefined,
@@ -57,19 +66,33 @@
             hashParameter: 'page'
         }, options || {});
 
+        if (!this._options.previousPageLabel) {
+            this._options.previousPageLabel = 'Previous ' + this._options.maxSize;
+        }
+
+        if (!this._options.nextPageLabel) {
+            this._options.nextPageLabel = 'Next ' + this._options.maxSize;
+        }
+
+
         this._handlers = {
             click: this._onClick.bindObjEvent(this)
         };
-
-        this._current = 0;
-        this._itemLiEls = [];
-        //_ulEl, _prevEl, _nextEl
 
         this._element = Aux.elOrSelector(selector, '1st argument');
 
         if (!Aux.isInteger(this._options.size)) {
             throw new TypeError('size option is a required integer!');
         }
+
+        if (!Aux.isInteger(this._options.start) && this._options.start > 0 && this._options.start <= this._options.size) {
+            throw new TypeError('start option is a required integer between 1 and size!');
+        }
+
+        if (this._options.maxSize && !Aux.isInteger(this._options.maxSize) && this._options.maxSize > 0) {
+            throw new TypeError('maxSize option is a positive integer!');
+        }
+
         else if (this._options.size < 0) {
             throw new RangeError('size option must be equal or more than 0!');
         }
@@ -77,6 +100,9 @@
         if (this._options.onChange !== undefined && typeof this._options.onChange !== 'function') {
             throw new TypeError('onChange option must be a function!');
         }
+
+        this._current = this._options.start - 1;
+        this._itemLiEls = [];
 
         this._init();
     };
@@ -129,6 +155,28 @@
                 this._itemLiEls = liEls;
             }
 
+            if (this._options.maxSize) {
+                // toggle visible items
+                var page = Math.floor( this._current / this._options.maxSize );
+                var pi = this._options.maxSize * page;
+                var pf = pi + this._options.maxSize - 1;
+
+                for (i = 0, f = this._options.size; i < f; ++i) {
+                    liEl = liEls[i];
+                    Css.setClassName(liEl, 'hide-all', i < pi || i > pf);
+                }
+
+                this._pageStart = pi;
+                this._pageEnd = pf;
+                this._page = page;
+
+                Css.setClassName(this._prevPageEl, 'disabled', !this.hasPreviousPage());
+                Css.setClassName(this._nextPageEl, 'disabled', !this.hasNextPage());
+
+                Css.setClassName(this._firstEl, 'disabled', this.isFirst());
+                Css.setClassName(this._lastEl, 'disabled', this.isLast());
+            }
+
             // update prev and next
             Css.setClassName(this._prevEl, 'disabled', !this.hasPrevious());
             Css.setClassName(this._nextEl, 'disabled', !this.hasNext());
@@ -148,10 +196,23 @@
             if( ( ulEl = Selector.select('ul.pagination',el)).length < 1 ){
                 ulEl = document.createElement('ul');
                 Css.addClassName(ulEl, 'pagination');
-
             } else {
                 hasUlAlready = true;
                 ulEl = ulEl[0];
+            }
+
+            if (this._options.maxSize) {
+                liEl = document.createElement('li');
+                liEl.appendChild( genAEl(this._options.firstLabel) );
+                this._firstEl = liEl;
+                Css.addClassName(liEl, 'first');
+                ulEl.appendChild(liEl);
+
+                liEl = document.createElement('li');
+                liEl.appendChild( genAEl(this._options.previousPageLabel) );
+                this._prevPageEl = liEl;
+                Css.addClassName(liEl, 'previousPage');
+                ulEl.appendChild(liEl);
             }
 
             liEl = document.createElement('li');
@@ -165,6 +226,20 @@
             this._nextEl = liEl;
             Css.addClassName(liEl, 'next');
             ulEl.appendChild(liEl);
+
+            if (this._options.maxSize) {
+                liEl = document.createElement('li');
+                liEl.appendChild( genAEl(this._options.nextPageLabel) );
+                this._nextPageEl = liEl;
+                Css.addClassName(liEl, 'nextPage');
+                ulEl.appendChild(liEl);
+
+                liEl = document.createElement('li');
+                liEl.appendChild( genAEl(this._options.lastLabel) );
+                this._lastEl = liEl;
+                Css.addClassName(liEl, 'last');
+                ulEl.appendChild(liEl);
+            }
 
             if( !hasUlAlready ){
                 el.appendChild(ulEl);
@@ -191,8 +266,21 @@
 
             var isPrev = Css.hasClassName(liEl, 'previous');
             var isNext = Css.hasClassName(liEl, 'next');
+            var isPrevPage = Css.hasClassName(liEl, 'previousPage');
+            var isNextPage = Css.hasClassName(liEl, 'nextPage');
+            var isFirst = Css.hasClassName(liEl, 'first');
+            var isLast = Css.hasClassName(liEl, 'last');
 
-            if (isPrev || isNext) {
+            if (isFirst) {
+                this.setCurrent(0);
+            }
+            else if (isLast) {
+                this.setCurrent(this._options.size - 1);
+            }
+            else if (isPrevPage || isNextPage) {
+                this.setCurrent( (isPrevPage ? -1 : 1) * this._options.maxSize, true);
+            }
+            else if (isPrev || isNext) {
                 this.setCurrent(isPrev ? -1 : 1, true);
             }
             else {
@@ -244,11 +332,11 @@
             this._current = nr;
             this._updateItems();
 
-            if (this._options.setHash) {
+            /*if (this._options.setHash) {
                 var o = {};
                 o[this._options.hashParameter] = nr;
                 Aux.setHash(o);
-            }
+            }*/
 
             if (this._options.onChange) { this._options.onChange(this); }
         },
@@ -293,6 +381,20 @@
          */
         hasNext: function() {
             return this._current < this._options.size - 1;
+        },
+
+        /**
+         * @function {Boolean} ? returns true iif has prior set of page(s)
+         */
+        hasPreviousPage: function() {
+            return this._options.maxSize && this._current > this._options.maxSize - 1;
+        },
+
+        /**
+         * @function {Boolean} ? returns true iif has set of page(s) ahead
+         */
+        hasNextPage: function() {
+            return this._options.maxSize && this._options.size - this._current >= this._options.maxSize + 1;
         },
 
         /**
