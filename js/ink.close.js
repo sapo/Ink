f88fa6e8e159e383b21606c34dd1fa829b0a5366
Fábio Santos <fabio-s-santos@telecom.pt>
2013-07-25 18:36:04 +0100

    updating Ink.UI.Close

--- js/ink.close.js
+++ js/ink.close.js
@@ -1,14 +1,13 @@
 /**
  * @module Ink.UI.Close_1
  * @author inkdev AT sapo.pt
- * @version 1
  */
-Ink.createModule('Ink.UI.Close', '1', ['Ink.Dom.Event_1','Ink.Dom.Css_1','Ink.Util.Array_1'], function(Event, Css, InkArray ) {
+Ink.createModule('Ink.UI.Close', '1', ['Ink.Dom.Event_1','Ink.Dom.Element_1'], function(InkEvent, InkElement) {
     'use strict';
 
     /**
      * Subscribes clicks on the document.body. If and only if you clicked on an element
-     * having class "ink-close", will go up the DOM hierarchy looking for an element with any
+     * having class "ink-close" or "ink-dismiss", will go up the DOM hierarchy looking for an element with any
      * of the following classes: "ink-alert", "ink-alert-block".
      * If it is found, it is removed from the DOM.
      * 
@@ -16,10 +15,8 @@ Ink.createModule('Ink.UI.Close', '1', ['Ink.Dom.Event_1','Ink.Dom.Css_1','Ink.Ut
      * 
      * @class Ink.UI.Close
      * @constructor
-     * @version 1
      * @uses Ink.Dom.Event
-     * @uses Ink.Dom.Css
-     * @uses Ink.Util.Array
+     * @uses Ink.Dom.Element
      * @example
      *     <script>
      *         Ink.requireModules(['Ink.UI.Close_1'],function( Close ){
@@ -28,23 +25,23 @@ Ink.createModule('Ink.UI.Close', '1', ['Ink.Dom.Event_1','Ink.Dom.Css_1','Ink.Ut
      *     </script>
      */
     var Close = function() {
+        InkEvent.observe(document.body, 'click', function(ev) {
+            var el = InkEvent.element(ev);
 
-        Event.observe(document.body, 'click', function(ev) {
-            var el = Event.element(ev);
-            if (!Css.hasClassName(el, 'ink-close') && !Css.hasClassName(el, 'ink-dismiss')) { return; }
+            el = InkElement.findUpwardsByClass(el, 'ink-close') ||
+                 InkElement.findUpwardsByClass(el, 'ink-dismiss');
 
-            var classes;
-            do { 
-                if (!el.className) { continue; }
-                classes = el.className.split(' ');
-                if (!classes) { continue; }
-                if ( InkArray.inArray('ink-alert',       classes) ||
-                     InkArray.inArray('ink-alert-block', classes) ) { break; }
-            } while ((el = el.parentNode));
+            if (!el) {
+                return;  // ink-close or ink-dismiss class not found
+            }
 
-            if (el) {
-                Event.stop(ev);
-                el.parentNode.removeChild(el);
+            var toRemove = el;
+            toRemove = InkElement.findUpwardsByClass(el, 'ink-alert') ||
+                       InkElement.findUpwardsByClass(el, 'ink-alert-block');
+
+            if (toRemove) {
+                InkEvent.stop(ev);
+                InkElement.remove(toRemove);
             }
         });
     };
