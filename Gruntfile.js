37da27bbd97f040a7175823dea06c931062b1428
Pedro Correia <pedro.a.correia@co.sapo.pt>
2013-09-26 13:21:47 +0100

    More travis testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,34 +3,25 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    // Compile Inks CSS
     less: {
-      css: {
+      dist: {
         files: {
-          "css/<%= pkg.name %>.css":"less/<%= pkg.name %>.less"
-        }
-      }
-      ,legacy_css: {
-        files: {
+          "css/<%= pkg.name %>.css":"less/<%= pkg.name %>.less",
           "css/<%= pkg.name %>-ie7.css":"less/<%= pkg.name %>-ie7.less"
         }
-      }
-      ,minified_css: {
+      },
+      // Compile Inks minified CSS
+      min: {
         options: {
           yuicompress: true
-        }
-        ,files: {
-          "css/<%= pkg.name %>-min.css":"less/<%= pkg.name %>.less"
-        }
-      }
-      ,minified_legacy_css: {
-        options: {
-          yuicompress: true
-        }
-        ,files: {
-          "css/<%= pkg.name %>-ie7-min.css":"less/<%= pkg.name %>-ie7.less"
+        },
+        files: {
+          "css/<%= pkg.name %>-min.css":"less/<%= pkg.name %>.less",
+          "css/<%= pkg.name %>-ie7-min.css":"less/<%= pkg.name %>-ie7.less"            
         }
       }
-    }    
+    }  
   });
 
   // Load the plugin that provides the "less" task.
@@ -38,6 +29,4 @@ module.exports = function(grunt) {
 
   // Default task(s).
   grunt.registerTask('default', ['less']);
-
-
 };
