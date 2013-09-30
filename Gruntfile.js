f8bb723848ff3209e92325117a6a6f9ee2348da9
Pedro Correia <pedro.a.correia@co.sapo.pt>
2013-09-30 11:34:59 +0100

    Some more tests with travis-ci config.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,11 +21,13 @@ module.exports = function(grunt) {
           "css/<%= pkg.name %>-ie7-min.css":"less/<%= pkg.name %>-ie7.less"            
         }
       }
-    }  
+    }
+
   });
 
   // Load the plugin that provides the "less" task.
   grunt.loadNpmTasks('grunt-contrib-less');
+  // grunt.loadNpmTasks('grunt-git-dist'); 
 
   // Default task(s).
   grunt.registerTask('default', ['less']);
