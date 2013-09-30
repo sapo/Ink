2d9bbfaebdab3a1c3d07f0661b2cef710cecf17d
Pedro Correia <pedro.a.correia@co.sapo.pt>
2013-09-30 16:56:26 +0100

    More work on the new site structure

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      // Compile Inks minified CSS
+      docs: {
+        options: {
+          yuicompress: true
+        },
+        files: {
+          "./css/docs.css":"./less/docs.less",
+          "./css/ink-ie7-min.css":"./less/ink-ie7.less"
+        }
+      }
+    },
+    jekyll: {
+      server: {
+        options: {
+          serve: true,
+          watch: true,
+          port: 4000
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "less" task.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  // Load the plugin that provides the "jekyll" task.
+  grunt.loadNpmTasks('grunt-jekyll');
+
+  // Default task(s).
+  grunt.registerTask('default', ['less']);
+};
