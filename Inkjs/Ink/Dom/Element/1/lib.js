9191672dc7f466f4cf5fd81ce1ab9cf5ec7e8747
Mário Gonçalves <mario.mc.goncalves@gmail.com>
2013-11-15 17:53:44 +0000

    Use getBoundingClientRect for a more precise way of calculating the element offsets

--- Inkjs/Ink/Dom/Element/1/lib.js
+++ Inkjs/Ink/Dom/Element/1/lib.js
@@ -197,34 +197,47 @@ Ink.createModule('Ink.Dom.Element', 1, [], function() {
             var bProp = ['border-left-width', 'border-top-width'];
             var res = [0, 0];
             var dRes, bRes, parent, cs;
-            var getPropPx = InkElement._getPropPx;
+            var getPropPx = this._getPropPx;
+            var box;
 
-            var InkBrowser = Ink.getModule('Ink.Dom.Browser', 1);
+            if ( "getBoundingClientRect" in document.documentElement ) {
+                var doc = el.ownerDocument,
+                    docElem = doc.documentElement,
+                    box = el.getBoundingClientRect(),
+                    body = doc.body,
+                    clientTop  = docElem.clientTop  || body.clientTop  || 0,
+                    clientLeft = docElem.clientLeft || body.clientLeft || 0,
+                    scrollTop  = doc.pageYOffset || docElem.scrollTop  || body.scrollTop,
+                    scrollLeft = doc.pageXOffset || docElem.scrollLeft || body.scrollLeft,
+                    top  = box.top  + scrollTop  - clientTop,
+                    left = box.left + scrollLeft - clientLeft;
+                res = [left, top];
+            } else {
+                var InkBrowser = Ink.getModule('Ink.Dom.Browser', 1);
+                do {
+                    cs = window.getComputedStyle ? window.getComputedStyle(el, null) : el.currentStyle;
+                    dRes = [el.offsetLeft | 0, el.offsetTop | 0];
 
-            do {
-                cs = window.getComputedStyle ? window.getComputedStyle(el, null) : el.currentStyle;
-                dRes = [el.offsetLeft | 0, el.offsetTop | 0];
-
+                    bRes = [getPropPx(cs, bProp[0]), getPropPx(cs, bProp[1])];
+                    if( InkBrowser.OPERA ){
+                        res[0] += dRes[0];
+                        res[1] += dRes[1];
+                    } else {
+                        res[0] += dRes[0] + bRes[0];
+                        res[1] += dRes[1] + bRes[1];
+                    }
+                    parent = el.offsetParent;
+                } while (el = parent);
                 bRes = [getPropPx(cs, bProp[0]), getPropPx(cs, bProp[1])];
-                if( InkBrowser.OPERA ){
-                    res[0] += dRes[0];
-                    res[1] += dRes[1];
-                } else {
-                    res[0] += dRes[0] + bRes[0];
-                    res[1] += dRes[1] + bRes[1];
+
+                if (InkBrowser.GECKO) {
+                    res[0] += bRes[0];
+                    res[1] += bRes[1];
                 }
-                parent = el.offsetParent;
-            } while (el = parent);
-
-            bRes = [getPropPx(cs, bProp[0]), getPropPx(cs, bProp[1])];
-
-            if (InkBrowser.GECKO) {
-                res[0] += bRes[0];
-                res[1] += bRes[1];
-            }
-            else if( !InkBrowser.OPERA ) {
-                res[0] -= bRes[0];
-                res[1] -= bRes[1];
+                else if( !InkBrowser.OPERA ) {
+                    res[0] -= bRes[0];
+                    res[1] -= bRes[1];
+                }
             }
 
             return res;
