a0473941a82ddc01756db9fd92caaa91bfeff740
Fábio Santos <fabio-s-santos@telecom.pt>
2013-12-12 21:30:09 +0000

    spying elements, per spy target. Also, only one onScroll listener per page. Also, no items are now highlighted when on the top of the page before any title.

--- src/js/Ink/UI/Spy/1/lib.js
+++ src/js/Ink/UI/Spy/1/lib.js
@@ -6,6 +6,102 @@
 Ink.createModule('Ink.UI.Spy', '1', ['Ink.UI.Common_1','Ink.Dom.Event_1','Ink.Dom.Css_1','Ink.Dom.Element_1','Ink.Dom.Selector_1'], function(Common, Event, Css, Element, Selector ) {
     'use strict';
 
+    // Maps a spy target (EG a menu with links inside) to spied instances.
+    var spyTargets = [
+        // [target, [spied, spied, spied...]], ...
+    ];
+
+    function targetIndex(target) {
+        for (var i = 0, len = spyTargets.length; i < len; i++) {
+            if (spyTargets[i][0] === target) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    function addSpied(spied, target) {
+        var index = targetIndex(target);
+
+        if (index === null) {
+            spyTargets.push([target, [spied]]);
+        } else {
+            spyTargets[index][1].push(spied);
+        }
+    }
+
+    var observingOnScroll = false;
+    function observeOnScroll() {
+        if (!observingOnScroll) {
+            observingOnScroll = true;
+            Event.observe(document, 'scroll', Event.throttle(onScroll, 300));
+        }
+    }
+
+    function onScroll() {
+        for (var i = 0, len = spyTargets.length; i < len; i++) {
+            onScrollForTarget(spyTargets[i][0], spyTargets[i][1]);
+        }
+    }
+
+    function onScrollForTarget(target, spied) {
+        var activeEl = findActiveElement(spied);
+
+        // This selector finds li's to deactivate
+        var toDeactivate = Selector.select('li.active', target);
+        for (var i = 0, total = toDeactivate.length; i < total; i++) {
+            Css.removeClassName(toDeactivate[i], 'active');
+        }
+
+        if (activeEl === null) {
+            return;
+        }
+
+        // The link which should be activated has a "href" ending with "#" + name or id of the element
+        var menuLinkSelector = 'a[href$="#' + (activeEl.name || activeEl.id) + '"]';
+
+        var toActivate = Selector.select(menuLinkSelector, target);
+        for (i = 0, total = toActivate.length; i < total; i++) {
+            Css.addClassName(Element.findUpwardsByTag(toActivate[i], 'li'), 'active');
+        }
+    }
+
+    function findActiveElement(spied) {
+        /* 
+         * Find the element above the top of the screen, but closest to it.
+         *          _____ 
+         *         |_____| element 1  (active element)
+         *
+         *                              ---
+         *          _____                 |
+         *         |     |  element 2     |    viewport visible area         
+         *         |     |                |
+         *         |_____|                |
+         *                              ---
+         */
+
+        // Remember that getBoundingClientRect returns coordinates
+        // relative to the top left corner of the screen.
+        //
+        // So checking if it's < 0 is used to tell if
+        // the element is above the top of the screen.
+        var closest = -Infinity;
+        var closestIndex;
+        var bBox;
+        for( var i = 0, total = spied.length; i < total; i++ ){
+            bBox = spied[i].getBoundingClientRect();
+            if (bBox.top <= 0 && bBox.top > closest) {
+                closest = bBox.top;
+                closestIndex = i;
+            }
+        }
+        if (closestIndex === undefined) {
+            return null;
+        } else {
+            return spied[closestIndex];
+        }
+    }
+
     /**
      * Spy is a component that 'spies' an element (or a group of elements) and when they leave the viewport (through the top),
      * highlight an option - related to that element being spied - that resides in a menu, initially identified as target.
@@ -16,7 +112,7 @@ Ink.createModule('Ink.UI.Spy', '1', ['Ink.UI.Common_1','Ink.Dom.Event_1','Ink.Do
      * @param {String|DOMElement} selector
      * @param {Object} [options] Options
      *     @param {DOMElement|String}     options.target          Target menu on where the spy will highlight the right option.
-     *     @param {String}                [options.activeClass='active'] Class which marks the "li" as active.
+     *     TODO @xparam {String}                [options.activeClass='active'] Class which marks the "li" as active.
      * @example
      *      <script>
      *          Ink.requireModules( ['Ink.Dom.Selector_1','Ink.UI.Spy_1'], function( Selector, Spy ){
@@ -51,72 +147,16 @@ Ink.createModule('Ink.UI.Spy', '1', ['Ink.UI.Common_1','Ink.Dom.Event_1','Ink.Do
     };
 
     Spy.prototype = {
-
-        /**
-         * Stores the spy elements
-         *
-         * @property _elements
-         * @type {Array}
-         * @readOnly
-         * 
-         */
-        _elements: [],  // [wtf] since arrays are immutable, _elements is the same for every instance. Very weird logic going on here
-
         /**
          * Init function called by the constructor
          * 
          * @method _init
          * @private
          */
-        _init: function(){
-            // Rate limited scroll function
-            var throttledScroll = Event.throttle(Ink.bind(this._onScroll, this), 300);
-            throttledScroll();
-            Event.observe( document, 'scroll', throttledScroll );
-            this._elements.push(this._rootElement);
-        },
-
-        /**
-         * Scroll handler. Responsible for highlighting the right options of the target menu.
-         * 
-         * @method _onScroll
-         * @private
-         */
-        _onScroll: function(){
-            var bbox = this._rootElement.getBoundingClientRect();
-
-            // To be the active element, its top must be above the viewport
-            // (so that the content is inside the viewport)
-            if( bbox.top > 0 ) {
-                return;
-            }
-
-            // Find other elements, check if any other element could be the active element
-            var otherBbox;
-            for( var i = 0, total = this._elements.length; i < total; i++ ){
-                if (this._elements[i] !== this._rootElement) {
-                    otherBbox = this._elements[i].getBoundingClientRect();
-                    if (otherBbox.top <= 0 && otherBbox.top > bbox.top) {
-                        return;
-                    }
-                }
-            }
-
-            // This selector finds li's to deactivate
-            var activeLinkSelector = 'li.active';  // [todo] configurable active class
-
-            // The link which should be activated has a "href" ending with "#" + name or id of the element
-            var menuLinkSelector = 'a[href$="#' + (this._rootElement.name || this._rootElement.id) + '"]';
-
-            var toDeactivate = Selector.select(activeLinkSelector, this._options.target);
-            for (i = 0, total = toDeactivate.length; i < total; i++) {
-                Css.removeClassName(toDeactivate[i], this._options.activeClass);
-            }
-
-            var toActivate = Selector.select(menuLinkSelector, this._options.target);
-            for (i = 0, total = toActivate.length; i < total; i++) {
-                Css.addClassName(Element.findUpwardsByTag(toActivate[i], 'li'), this._options.activeClass);
-            }
+        _init: function() {
+            addSpied(this._rootElement, this._options.target);
+            observeOnScroll();
+            onScroll();
         }
     };
 
