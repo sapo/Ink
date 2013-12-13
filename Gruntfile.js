3507dcb43c1697a2d43d0e5ce7b6759aa555b9da
Mário Gonçalves <mario.mc.goncalves@gmail.com>
2013-12-13 12:20:01 +0000

    Adding source maps to both LESS and JS compilation

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,22 +27,22 @@ module.exports = function(grunt) {
     copy: {
       fontAwesome: {
         files: [
-          {
-            cwd: 'tmp/font-awesome/less/',
-            src: '*.less', 
-            dest: 'src/less/modules/icons/',
-            expand: true,
-          }
+        {
+          cwd: 'tmp/font-awesome/less/',
+          src: '*.less', 
+          dest: 'src/less/modules/icons/',
+          expand: true,
+        }
         ]
       },
       modernizr: {
         files: [
-          {
-            cwd: 'tmp/modernizr',
-            src: 'modernizr.js', 
-            dest: '<%= ink.folders.js.dist %>',
-            expand: true,
-          }
+        {
+          cwd: 'tmp/modernizr',
+          src: 'modernizr.js', 
+          dest: '<%= ink.folders.js.dist %>',
+          expand: true,
+        }
         ]
       }
     },
@@ -73,17 +73,17 @@ module.exports = function(grunt) {
           flatten: true,
           cwd: '<%= ink.folders.js.src %>',
           src: [
-            '1/**/lib.js',
-            'Net/**/lib.js',
-            'Dom/**/lib.js',
-            'Util/**/lib.js',
+          '1/**/lib.js',
+          'Net/**/lib.js',
+          'Dom/**/lib.js',
+          'Util/**/lib.js',
           ],
           dest: '<%= ink.folders.js.dist %>',
           rename: function(dest, src) {
             return dest + 'ink.js';
           },
         },
-       ],
+        ],
       },
 
       // ink-all.js
@@ -94,47 +94,47 @@ module.exports = function(grunt) {
           flatten: true,
           cwd: '<%= ink.folders.js.src %>',
           src: [
-            '1/**/lib.js',
-            'Net/**/lib.js',
-            'Dom/**/lib.js',
-            'Util/**/lib.js',
-            'UI/**/lib.js'
+          '1/**/lib.js',
+          'Net/**/lib.js',
+          'Dom/**/lib.js',
+          'Util/**/lib.js',
+          'UI/**/lib.js'
           ],
           dest: '<%= ink.folders.js.dist %>',
           rename: function(dest, src) {
             return dest + 'ink-all.js';
           },
         },
-       ],
+        ],
       },
 
       // ink-ui.js
       ink_ui: {
         files: [
-          {
-            expand: true,
-            flatten: true,
-            cwd: '<%= ink.folders.js.src %>UI/',
-            src: [
-              '**/lib.js'
-            ],
-            dest: '<%= ink.folders.js.dist %>',
-            rename: function(dest, src) {
-              return dest + 'ink-ui.js';
-            },
+        {
+          expand: true,
+          flatten: true,
+          cwd: '<%= ink.folders.js.src %>UI/',
+          src: [
+          '**/lib.js'
+          ],
+          dest: '<%= ink.folders.js.dist %>',
+          rename: function(dest, src) {
+            return dest + 'ink-ui.js';
           },
+        },
         ],
       },
 
       // ui components as single files
       ui: {
         files: [
-          {
-            expand: true,
-            cwd: '<%= ink.folders.js.src %>UI/',
-            src: ['**/[0-9]/lib.js'],
-            dest: '<%= ink.folders.js.dist %>',
-            rename: function(dest, src) {
+        {
+          expand: true,
+          cwd: '<%= ink.folders.js.src %>UI/',
+          src: ['**/[0-9]/lib.js'],
+          dest: '<%= ink.folders.js.dist %>',
+          rename: function(dest, src) {
               // [TODO] refactor
               // check if this is v1
               if (src.substring(src.lastIndexOf('/'),-1).match(/[0-9]/) && src.substring(src.lastIndexOf('/'),-1).match(/[0-9]/) == 1) {
@@ -146,28 +146,28 @@ module.exports = function(grunt) {
               }
             },
           },
-        ],
+          ],
+        },
       },
-    },
 
-    clean: {
-      js: {
-        src: ["<%= ink.folders.js.dist %>/ink*.js"]
+      clean: {
+        js: {
+          src: ["<%= ink.folders.js.dist %>/ink*.js"]
+        },
+        css: {
+          src: ["<%= ink.folders.css.dist %>/ink*.css"]
+        },
+        fontAwesome: {
+          src: ["<%= ink.folders.css.src %>/less/modules/icons/*.less"]
+        },
+        tmp: {
+          src: ["tmp"]
+        }
       },
-      css: {
-        src: ["<%= ink.folders.css.dist %>/ink*.css"]
-      },
-      fontAwesome: {
-        src: ["<%= ink.folders.css.src %>/less/modules/icons/*.less"]
-      },
-      tmp: {
-        src: ["tmp"]
-      }
-    },
 
     // [TODO] check if this works okay
     qunit: {
-        options: {
+      options: {
           // inject: 'js/tests/assets/phantom.js',
           urls: ['http://localhost:8000/js/tests/index.html']
         },
@@ -188,6 +188,7 @@ module.exports = function(grunt) {
     uglify: {
       options: {
         report: "min",
+        sourceMapRoot: '../..',
         compress: {
           sequences: true,
           properties: true,
@@ -208,36 +209,69 @@ module.exports = function(grunt) {
         }
       },
       ink: {
-        src: '<%= ink.folders.js.dist %>ink.js', 
-        dest: '<%= ink.folders.js.dist %>ink.min.js'
+        src: '<%= ink.folders.js.dist %><%= pkg.name %>.js',
+        options: {
+          sourceMap: '<%= ink.folders.js.dist %><%= pkg.name %>.js.map',
+          sourceMappingURL: '<%= pkg.name %>.js.map'
+        },
+        dest: '<%= ink.folders.js.dist %><%= pkg.name %>.min.js'
       },
       ink_all: {
-        src: '<%= ink.folders.js.dist %>ink-all.js', 
-        dest: '<%= ink.folders.js.dist %>ink-all.min.js'
+        src: '<%= ink.folders.js.dist %><%= pkg.name %>-all.js',
+        options: {
+          sourceMap: '<%= ink.folders.js.dist %><%= pkg.name %>-all.js.map',
+          sourceMappingURL: '<%= pkg.name %>-all.js.map'
+        }, 
+        dest: '<%= ink.folders.js.dist %><%= pkg.name %>-all.min.js'
       },
       ink_ui: {
-        src: '<%= ink.folders.js.dist %>ink-ui.js', 
-        dest: '<%= ink.folders.js.dist %>ink-ui.min.js'
-      },
+        src: '<%= ink.folders.js.dist %><%= pkg.name %>-ui.js',
+        options: {
+          sourceMap: '<%= ink.folders.js.dist %><%= pkg.name %>-ui.js.map',
+          sourceMappingURL: '<%= pkg.name %>-ui.js.map'
+        }, 
+        dest: '<%= ink.folders.js.dist %><%= pkg.name %>-ui.min.js'
+      }
     },
 
     // COMPILES THE CSS
     less: {
-      dist: {
+      distMin: {
         files: {
-          '<%= ink.folders.css.dist %><%= pkg.name %>.css':['<%= ink.folders.css.src %><%= pkg.name %>.less'],
-          '<%= ink.folders.css.dist %><%= pkg.name %>-ie7.css':['<%= ink.folders.css.src %><%= pkg.name %>-ie7.less']
+          '<%= ink.folders.css.dist %><%= pkg.name %>-min.css':['<%= ink.folders.css.src %><%= pkg.name %>.less'],
+        },
+          // COMPILES THE MINIFIED CSS
+          options: {
+            compress: true,
+            // LESS source maps
+            // To enable, set sourceMap to true and update sourceMapRootpath based on your install
+            sourceMap: true,
+            sourceMapFilename: '<%= ink.folders.css.dist %><%= pkg.name %>-min.css.map',
+            sourceMapRootpath: '../../'
+          }
+        },
+        IEMin: {
+          files: {
+            '<%= ink.folders.css.dist %><%= pkg.name %>-ie7-min.css':['<%= ink.folders.css.src %><%= pkg.name %>-ie7.less']
+          }
+        },
+        dist: {
+          files: {
+            '<%= ink.folders.css.dist %><%= pkg.name %>.css':['<%= ink.folders.css.src %><%= pkg.name %>.less'],
+          },
+        // COMPILES THE MINIFIED CSS
+        options: {
+          compress: false,
+          // LESS source maps
+          // To enable, set sourceMap to true and update sourceMapRootpath based on your install
+          sourceMap: true,
+          sourceMapFilename: '<%= ink.folders.css.dist %><%= pkg.name %>.css.map',
+          sourceMapRootpath: '../../'
         }
       },
-
-      // COMPILES THE MINIFIED CSS
-      min: {
-        options: {
-          yuicompress: true
-        },
+      IE: {
         files: {
-          '<%= ink.folders.css.dist %><%= pkg.name %>-min.css':'<%= ink.folders.css.src %><%= pkg.name %>.less',
-          '<%= ink.folders.css.dist %><%= pkg.name %>-ie7-min.css':'<%= ink.folders.css.src %><%= pkg.name %>-ie7.less'
+          '<%= ink.folders.css.dist %><%= pkg.name %>-ie7.css':['<%= ink.folders.css.src %><%= pkg.name %>-ie7.less']
         }
       }
     },
@@ -269,46 +303,46 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-bower-task');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-plato');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-contrib-qunit');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-bower-task');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-plato');
 
-  grunt.registerTask('default', ['bower', 'copy', 'clean:css', 'less','clean:js','concat','uglify', 'clean:tmp']);
-  grunt.registerTask('test', ['connect', 'qunit']);
-  grunt.registerTask('custom_bundle', 'Create your custom bundle from a json file', function(fileName){
-    if (arguments.length === 0) {
-      grunt.log.error('You need to specify a file name');
+grunt.registerTask('default', ['bower', 'copy', 'clean:css', 'less','clean:js','concat','uglify', 'clean:tmp']);
+grunt.registerTask('test', ['connect', 'qunit']);
+grunt.registerTask('custom_bundle', 'Create your custom bundle from a json file', function(fileName){
+  if (arguments.length === 0) {
+    grunt.log.error('You need to specify a file name');
+  }
+
+  var bundle = grunt.file.readJSON(fileName);
+  var dependencies = bundle.dependencies || [];
+  grunt.config.set('concat.ink_custom', {
+   files: [
+   {
+    expand: true,
+    flatten: true,
+    cwd: '<%= ink.folders.js.src %>',
+    dest: '<%= ink.folders.js.dist %>' + bundle['name'] + '/',
+    src: dependencies.map(function(depName){
+      return depName.replace('Ink/', '') + '/lib.js';
+    }),
+    rename: function(dest, src) {
+      return dest + 'ink-custom.js';
     }
-    
-    var bundle = grunt.file.readJSON(fileName);
-    var dependencies = bundle.dependencies || [];
-    grunt.config.set('concat.ink_custom', {
-         files: [
-          {
-            expand: true,
-            flatten: true,
-            cwd: '<%= ink.folders.js.src %>',
-            dest: '<%= ink.folders.js.dist %>' + bundle['name'] + '/',
-            src: dependencies.map(function(depName){
-                return depName.replace('Ink/', '') + '/lib.js';
-            }),
-            rename: function(dest, src) {
-              return dest + 'ink-custom.js';
-            },
-          },
-        ],       
-    });
-    grunt.config.set('uglify.ink_custom', {
-        src: '<%= ink.folders.js.dist %>' + bundle['name'] + '/' + 'ink-custom.js',
-        dest: '<%= ink.folders.js.dist %>' + bundle['name'] + '/' + 'ink-custom.min.js'
-    });
-    grunt.task.run(['concat:ink_custom', 'uglify:ink_custom']);
+  }
+  ]
+});
+  grunt.config.set('uglify.ink_custom', {
+    src: '<%= ink.folders.js.dist %>' + bundle['name'] + '/' + 'ink-custom.js',
+    dest: '<%= ink.folders.js.dist %>' + bundle['name'] + '/' + 'ink-custom.min.js'
   });
+  grunt.task.run(['concat:ink_custom', 'uglify:ink_custom']);
+});
 };
