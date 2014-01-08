9e0e4bf9f8a379a941fc9b481645634393d4d03d
Pedro Correia <pedro.a.correia@co.sapo.pt>
2014-01-08 14:55:29 +0000

    Adds compass config.rb

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     less: {
       // Compile Inks minified CSS
       docs: {
@@ -15,6 +16,7 @@ module.exports = function(grunt) {
         }
       }
     },
+
     jekyll: {
       server: {
         options: {
@@ -24,6 +26,39 @@ module.exports = function(grunt) {
         }
       }
     },
+
+    compass: {                  
+      css: {                   
+        options: {   
+          config: "config.rb"
+        }
+      },
+    },
+
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: '<%= ink.folders.css.dist %>',
+        src: ['*.css', '!quick-start.css'],
+        dest: '<%= ink.folders.css.dist %>',
+        ext: '.min.css',
+        options: {
+          report: 'min'
+        }
+      }
+    },
+
+    watch: {
+      css: {
+        files: ['<%= ink.folders.css.src %>**/*.scss'],
+        tasks: ['css'],
+        options: {
+          spawn: false,
+          // interrupt: true,
+        }
+      }
+    },
+
     watch: {
       scripts: {
         files: ['less/**/*.less'],
@@ -35,11 +70,13 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-jekyll');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
   grunt.registerTask('default', ['watch']);
-  grunt.registerTask('all', ['jekyll','less']);
+  grunt.registerTask('all', ['compass','less']);
 };
---
+++ config.rb
@@ -0,0 +1,9 @@
+# General
+output_style = :expanded
+relative_assets = true
+project_path = "./"
+sass_dir = 'src/sass'
+css_dir = "dist/css"
+fonts_dir = 'dist/fonts'
+images_dir = 'dist/img'
+
