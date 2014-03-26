02c4f962edc6d1c0cf078f154a224d89c1b34ef3
Pedro Correia <pedro.a.correia@co.sapo.pt>
2014-03-26 14:23:18 +0000

    Moves floats to the grid classes to avoid collisions with other breakpoint prefixed classes.

--- config.rb
+++ config.rb
@@ -1,10 +1,9 @@
 # General
-output_style = :nested
-relative_assets = true
+output_style = :expanded
 line_comments = false
+relative_assets = true
 project_path = "./"
 sass_dir = 'src/sass'
 css_dir = "dist/css"
 fonts_dir = 'dist/fonts'
 images_dir = 'dist/img'
-
