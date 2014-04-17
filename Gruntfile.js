15ddc2caaa52567f3cc0f1dd2515eb90eef9f85c
Pedro Correia <pedro.a.correia@co.sapo.pt>
2014-04-17 16:53:26 +0100

    Removes unnecessary dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,42 +46,15 @@ module.exports = function(grunt) {
       },
 
       ink_all: {
-        src: '<%= ink.folders.js.dist %>ink-all.js',
-        dest: '<%= ink.folders.js.dist %>ink-all.min.js'
+        src: '<%= ink.folders.js.dist %>search.js',
+        dest: '<%= ink.folders.js.dist %>search.min.js'
       }
     },
 
-    concat: {
-      ink_all: {
-        files: [
-        {
-          expand: true,
-          flatten: true,
-          cwd: '<%= ink.folders.js.src %>',
-          src: [
-            '1/**/lib.js',
-            'Net/**/lib.js',
-            'Dom/**/lib.js',
-            'Util/**/lib.js',
-            'UI/**/lib.js'
-          ],
-          dest: '<%= ink.folders.js.dist %>',
-          rename: function(dest, src) {
-            return dest + 'ink-all.js';
-          },
-        },
-       ],
-      },
-    },
-
     clean: {
-      js: {
-        src: ["<%= ink.folders.js.dist %>/ink*.js"]
-      },
       css: {
         src: ["<%= ink.folders.css.dist %>/*.css","<%= ink.folders.css.dist %>/contrib" ]
       },
-      csscontrib: [ '<%= ink.folders.css.dist %>/contrib' ],
       inkdoc: ['_includes/js']
     },
 
@@ -117,15 +90,6 @@ module.exports = function(grunt) {
           // interrupt: true,
         }
       },
-      js: {
-        files: ['src/js/**/*.js'],
-        tasks: ['js'],
-        options: {
-          atBegin: true,
-          spawn: false,
-          // interrupt: true,
-        }
-      },
       inkdoc: {
         files: ['_includes/inkdoctemplates/ink/**/*.js','_includes/inkdoctemplates/ink/**/*.hbs'],
         tasks: ['shell:inkdoc', 'copy:inkdoc'],
@@ -138,14 +102,6 @@ module.exports = function(grunt) {
     },
 
     shell: {
-        src: {
-          options: {
-            stdout: true,
-            stderr: true,
-            failOnError: true
-          },
-          command: 'git remote update; git checkout origin/develop -- src && git add src'
-        },
         inkdoc: {
             options: {
                 stdout: true,
@@ -156,26 +112,7 @@ module.exports = function(grunt) {
         }
     },
 
-    text_grab: {
-      glossary: {
-        options: {
-          pattern: '\\.[a-zA-Z][a-zA-Z0-9-_]+',
-          templateStart: '<table class="props css">\n<tr><th class="large-30">Class</th><th>Description</th></tr>\n<caption>Glossary</caption>\n',
-          templateRow: '<tr>\n<td><code>%s</code></td>\n<td></td>\n</tr>\n',
-          templateEnd: '</table>\n',
-          exceptions: ['\\.fa','ttf','otf','svg','eot','woff','jpg','jpeg','png','Microsoft','Alpha'],
-        },
-       files: {
-         'glossary/g.html': ['assets/css/ink-flex.css']
-       }
-      },
-    },
-
     copy: {
-      facss: {
-        src: 'assets/css/contrib/font-awesome/font-awesome.css',
-        dest: 'assets/css/font-awesome.css'
-      },
       inkdoc: {
         files: [
           {
@@ -226,21 +163,17 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-jekyll');
   grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-text-grab');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['update','css','js']);
-  grunt.registerTask('update', ['shell:src']);
+  grunt.registerTask('default', ['css']);
   grunt.registerTask('docs', ['inkdoc','jekyll']);
   grunt.registerTask('dev', ['watch']);
   grunt.registerTask('inkdoc', ['shell:inkdoc', 'copy:inkdoc', 'clean:inkdoc']);
-  grunt.registerTask('js', ['clean:js','concat','uglify']);
-  grunt.registerTask('css', ['clean:css','compass','copy:facss','clean:csscontrib','cssmin']);
+  grunt.registerTask('css', ['clean:css','compass','cssmin']);
 };
