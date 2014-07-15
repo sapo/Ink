cef7d9b251818a660ae792a8f24ad775296fd051
Fábio Santos <fabio-s-santos@telecom.pt>
2014-07-15 11:42:04 +0100

    accidentally broke getInstanceFromSelector

--- src/js/Ink/UI/Common/1/lib.js
+++ src/js/Ink/UI/Common/1/lib.js
@@ -753,7 +753,7 @@ Ink.createModule('Ink.UI.Common', '1', ['Ink.Dom.Element_1', 'Ink.Net.Ajax_1','I
          * @return  {Object|Array}               Returns an instance or a collection of instances.
          */
         getInstanceFromSelector: function(selector) {
-            return Common.getInstance(Common.elOrSelector(el));
+            return Common.getInstance(Common.elOrSelector(selector));
         },
 
         /**
