08ce1dc4239c23dbb4b8b906d471f59aca802999
Fábio Santos <fabio-s-santos@telecom.pt>
2014-10-09 17:08:54 +0100

    more FormValidator tests

--- test/unit/Ink.UI.FormValidator_2/test.js
+++ test/unit/Ink.UI.FormValidator_2/test.js
@@ -2,25 +2,89 @@
 Ink.requireModules(['Ink.UI.FormValidator_2', 'Ink.Dom.Element_1', 'Ink.Dom.Selector_1', 'Ink.Util.Array_1', 'Ink.Dom.Event_1'], function (FormValidator, InkElement, Selector, InkArray, InkEvent) {
     'use strict';
 
-    test('(regression): FormValidator_2 should destroy FormElement instances after using them', function () {
-        document.body.insertAdjacentHTML('beforeend',
-              '<form id="myform" class="ink-form" method="post" action="http://local">'
-                + '<div id="element_25" class="control-group required validation error" data-rules="">'
-                    + '<div class="control">'
-                        + '<input id="radio1" type="ttradio">'
-                    + '</div>'
-                + '</div>'
-            + '</form>'
-        );
+    function makeForm(options) {
+        options = Ink.extendObj({
+            makeValidator: true
+        }, options || {});
 
-        var myForm = document.getElementById('myform')
-        var validator = new FormValidator(myForm)
+        var form = document.body.appendChild(InkElement.create('form', {
+            className: 'ink-form',
+            method: 'POST',
+            action: 'http://local'
+        }));
 
-        var initFunc = sinon.spy(FormValidator.FormElement.prototype, '_init');
+        var controlGroup = form.appendChild(InkElement.create('div', {
+            id: 'element_26',
+            className: 'control-group required',
+            'data-rules': 'required'
+        }));
+
+        var control = controlGroup.appendChild(InkElement.create('div', {
+            className: 'control'
+        }));
+
+        var newInput = control.appendChild(InkElement.create('input', {
+            id: 'radio2',
+            type: 'radio'
+        }));
+
+        if (options.makeValidator) {
+            var validator = new FormValidator(form)
+        }
+        
+        return {
+            form: form,
+            validator: validator || null,
+            controlGroup: controlGroup,
+            control: control
+        }
+    }
+
+    test('(regression): should destroy FormElement instances after using them', sinon.test(function () {
+        var o = makeForm()
+
+        var myForm = o.form
+        var validator = o.validator
+
+        var initFunc = this.spy(FormValidator.FormElement.prototype, '_init');
         validator.validate();
         validator.validate();
         equal(initFunc.callCount, 1, 'FormElement#_init only called once');
 
-        myForm.parentNode.removeChild(myForm)
-    });
+        myForm.parentNode.removeChild(myForm);
+    }));
+
+    test('Create new FormElements for each control-group in the form', sinon.test(function () {
+        var oldStuff = makeForm();
+
+        var myForm = oldStuff.form;
+        var validator = oldStuff.validator;
+
+        var initFunc = this.spy(FormValidator.FormElement.prototype, '_init');
+
+        validator.validate();
+        equal(initFunc.callCount, 1, 'FormElement#_init called for the existing control-group');
+
+        strictEqual(initFunc.firstCall &&
+            initFunc.firstCall.thisValue.getElement(),
+            oldStuff.controlGroup,
+            'FormElement was called with the correct element as argument');
+
+        var newStuff = makeForm();
+
+        newStuff.form.parentNode.removeChild(newStuff.form);
+        newStuff.controlGroup.id = 'element_27';
+        myForm.appendChild(newStuff.controlGroup);
+
+        validator.validate();
+
+        equal(initFunc.callCount, 2, 'FormElement#_init called again');
+
+        strictEqual(initFunc.lastCall &&
+            initFunc.lastCall.thisValue.getElement(),
+            newStuff.controlGroup,
+            'FormElement was now called with the new element as argument');
+
+        myForm.parentNode.removeChild(myForm);
+    }));
 });
