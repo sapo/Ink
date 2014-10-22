f6b7b4e43d6e96272706d96998bc61dc74adfd60
Fábio Santos <fabio-s-santos@telecom.pt>
2014-10-22 16:30:33 +0100

    deprecating unused method

--- src/js/Ink/UI/Common/1/lib.js
+++ src/js/Ink/UI/Common/1/lib.js
@@ -363,6 +363,7 @@ Ink.createModule('Ink.UI.Common', '1', ['Ink.Dom.Element_1', 'Ink.Net.Ajax_1','I
          *
          * @method clone
          * @static
+         * @deprecated
          * @param  {Object} o The object to be cloned/copied.
          * @return {Object} Returns the result of the clone/copy.
          * @example
