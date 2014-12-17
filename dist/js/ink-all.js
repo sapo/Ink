9f934bf2cedc0ab94a4aa82db7aa53a87ac33444
Pedro Correia <pedro.a.correia@co.sapo.pt>
2014-12-17 17:47:42 +0000

    Updates dist files.

--- dist/js/ink-all.js
+++ dist/js/ink-all.js
@@ -15351,17 +15351,17 @@ Ink.createModule('Ink.UI.DatePicker', '1', ['Ink.UI.Common_1','Ink.Dom.Event_1',
         },
 
         _listenToContainerObjectEvents: function () {
-            Event.observe(this._containerObject,'mouseover',Ink.bindEvent(function(e){
+            Event.observe(this._containerObject, 'mouseover' ,Ink.bindEvent(function(e){
                 Event.stop( e );
                 this._hoverPicker = true;
             },this));
 
-            Event.observe(this._containerObject,'mouseout',Ink.bindEvent(function(e){
+            Event.observe(this._containerObject, 'mouseout', Ink.bindEvent(function(e){
                 Event.stop( e );
                 this._hoverPicker = false;
             },this));
 
-            Event.observe(this._containerObject,'click',Ink.bindEvent(this._onClick, this));
+            Event.observe(this._containerObject, 'click', Ink.bindEvent(this._onClick, this));
         },
 
         _onClick: function(e){
@@ -23625,8 +23625,8 @@ Ink.createModule("Ink.UI.TagField","1",["Ink.Dom.Element_1", "Ink.Dom.Event_1", 
             // Boolean option handling
             this._options.closeOnClick = this._options.closeOnClick;
             // Actually a throolean
-            if (this._options.initialState === null){
-                Css.hasClassName(this._targets[0], this._options.classNameOn) ? this._options.initialState = true : this._options.initialState = false;
+            if (this._options.initialState === null) {
+                this._options.initialState = Css.hasClassName(this._targets[0], this._options.classNameOn);
             }
 
             if (this._options.classNameOn !== 'show-all' || this._options.classNameOff !== 'hide-all') {
