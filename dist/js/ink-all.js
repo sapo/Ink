f22e76b1d123900e06c243f1e77b701a1db20e62
Pedro Correia <pedro.a.correia@co.sapo.pt>
2015-02-11 12:38:59 +0000

    Updates dist files.

--- dist/js/ink-all.js
+++ dist/js/ink-all.js
@@ -15330,7 +15330,7 @@ Ink.createModule('Ink.UI.DatePicker', '1', ['Ink.UI.Common_1','Ink.Dom.Event_1',
 
             var parentIsControl = Selector.matchesSelector(
                 this._element.parentNode,
-                '.ink-form .control-group .control');
+                '.ink-form .control-group .control, .ink-form .control-group .control > *');
 
             if (parentIsControl) {
                 this._wrapper = this._element.parentNode;
